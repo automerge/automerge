8637a2c045df71edfeea262f94fea06083c8a15d
Alex Good <alex@memoryandthought.me>
2019-12-28 15:41:30 +0000

    Add module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! Get your changes from the javascript library like so:
+//!
+//! ```javascript
+//! doc = ... // create and edit an automerge document
+//! let changes = Automerge.getHistory(doc).map(h => h.change)
+//! console.log(JSON.stringify(changes, null, 4))
+//! ```
+//!
+//! Then load the changes in rust:
+//!
+//! ```rust,no_run
+//! # use automerge::Change;
+//! let changes_str = "<paste the contents of the output here>";
+//! let changes: Vec<Change> = serde_json::from_str(changes_str).unwrap();
+//! let doc = automerge::Document::load(changes).unwrap();
+//! println!("{:?}", doc.state().unwrap());
+//! ```
 mod document;
 mod error;
 mod op_set;
