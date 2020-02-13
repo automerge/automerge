2afcd1bbb698eee1f83b05873482d71ee934d1ed
Alex Good <alex@memoryandthought.me>
2020-02-13 13:28:44 +0000

    Stub out backend interface

---
+++ automerge-backend/src/error.rs
@@ -0,0 +1,42 @@
+use crate::protocol::ObjectID;
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum AutomergeError {
+    DuplicateObjectError,
+    MissingObjectError(ObjectID),
+    InvalidLinkTarget,
+    NotImplemented(String),
+    InvalidChange(String),
+}
+
+impl fmt::Display for AutomergeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl Error for AutomergeError {}
+
+#[derive(Debug)]
+pub struct InvalidElementID(pub String);
+
+impl fmt::Display for InvalidElementID {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl Error for InvalidElementID {}
+
+#[derive(Debug)]
+pub struct InvalidChangeRequest(pub String);
+
+impl Error for InvalidChangeRequest {}
+
+impl fmt::Display for InvalidChangeRequest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
--- automerge-backend/src/lib.rs
+++ automerge-backend/src/lib.rs
@@ -1,3 +1,52 @@
+mod protocol;
+mod error;
+
+use crate::protocol::{Change, ActorID, Clock};
+
+pub struct Backend  {
+}
+
+pub struct Patch {}
+
+impl Backend {
+    pub fn init() -> Backend {
+        Backend{}
+    }
+
+    pub fn apply_changes(&mut self, _changes: Vec<Change>) -> Patch {
+        Patch{}
+    }
+
+    pub fn apply_local_change(&mut self, _change: Change) -> Patch {
+        Patch{}
+    }
+
+    pub fn get_patch(&self) -> Patch {
+        Patch{}
+    }
+    
+    pub fn get_changes(&self) -> Vec<Change> {
+        Vec::new()
+    }
+
+    pub fn get_changes_for_actor_id(&self, _actor_id: ActorID) -> Vec<Change> {
+        Vec::new()
+    }
+
+    pub fn get_missing_changes(&self, _clock: Clock) -> Vec<Change> {
+        Vec::new()
+    }
+
+    pub fn get_missing_deps(&self) -> Clock {
+        Clock::empty()
+    }
+
+    pub fn merge(&mut self, _remote: &Backend) -> Patch {
+        Patch{}
+    }
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
