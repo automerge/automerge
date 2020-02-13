a0e3a433ca32e5f3c8881be33941e9839ceb66ba
Orion Henry <orion.henry@gmail.com>
2020-02-13 18:59:10 -0500

    fix snake case

--- automerge-backend-wasm/src/lib.rs
+++ automerge-backend-wasm/src/lib.rs
@@ -25,77 +25,60 @@ extern "C" {
 #[derive(PartialEq,Debug, Clone)]
 pub struct State { backend: Rc<RefCell<Backend>> }
 
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn applyChanges(state: &State, changes: JsValue) -> Array {
+#[wasm_bindgen(js_name = applyChange)]
+pub fn apply_changes(state: &State, changes: JsValue) -> Array {
   let c: Vec<Change> = changes.into_serde().unwrap();
-  let newState = state.clone();
-  let patch = newState.backend.borrow_mut().apply_changes(c);
+  let new_state = state.clone();
+  let patch = new_state.backend.borrow_mut().apply_changes(c);
   let ret = Array::new();
-  ret.push(&newState.into());
+  ret.push(&new_state.into());
   ret.push(&JsValue::from_serde(&patch).ok().into());
   ret
 }
 
-#[wasm_bindgen]
-pub struct Bar { }
-
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn foo() -> Bar {
-  Bar {}
-}
-
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn applyLocalChange(state: &State, change: JsValue) -> Array {
+#[wasm_bindgen(js_name = applyLocalChange)]
+pub fn apply_local_change(state: &State, change: JsValue) -> Array {
   let c: Change = change.into_serde().unwrap();
-  let newState = state.clone();
-  let patch = newState.backend.borrow_mut().apply_local_change(c);
+  let new_state = state.clone();
+  let patch = new_state.backend.borrow_mut().apply_local_change(c);
   let ret = Array::new();
-  ret.push(&newState.into());
+  ret.push(&new_state.into());
   ret.push(&JsValue::from_serde(&patch).ok().into());
   ret
 }
 
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn getPatch(state: &State) -> JsValue {
+#[wasm_bindgen(js_name = getPatch)]
+pub fn get_patch(state: &State) -> JsValue {
   let patch = state.backend.borrow().get_patch();
   JsValue::from_serde(&patch).ok().into()
 }
 
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn getChanges(state: &State) -> JsValue {
+#[wasm_bindgen(js_name = getChanges)]
+pub fn get_changes(state: &State) -> JsValue {
   let changes = state.backend.borrow().get_changes();
   JsValue::from_serde(&changes).ok().into()
 }
 
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn getChangesForActorId(state: &State, actorId: JsValue) -> JsValue {
-  let a: ActorID = actorId.into_serde().unwrap();
+#[wasm_bindgen(js_name = getChangesForActorId)]
+pub fn get_changes_for_actorid(state: &State, actorid: JsValue) -> JsValue {
+  let a: ActorID = actorid.into_serde().unwrap();
   let changes = state.backend.borrow().get_changes_for_actor_id(a);
   JsValue::from_serde(&changes).ok().into()
 }
 
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn getMissingChanges(state: &State, clock: JsValue) -> JsValue {
+#[wasm_bindgen(js_name = getMissingChanges)]
+pub fn get_missing_changes(state: &State, clock: JsValue) -> JsValue {
   let c: Clock = clock.into_serde().unwrap();
   let changes = state.backend.borrow().get_missing_changes(c);
   JsValue::from_serde(&changes).ok().into()
 }
 
-#[allow(non_snake_case)]
-#[wasm_bindgen]
-pub fn getMissingDeps(state: &State) -> JsValue {
+#[wasm_bindgen(js_name = getMissingDeps)]
+pub fn get_missing_deps(state: &State) -> JsValue {
   let clock = state.backend.borrow().get_missing_deps();
   JsValue::from_serde(&clock).ok().into()
 }
 
-#[allow(non_snake_case)]
 #[wasm_bindgen]
 pub fn merge(state: &mut State, remote: State) -> JsValue {
   let patch = state.backend.borrow_mut().merge(&remote.backend.borrow());
