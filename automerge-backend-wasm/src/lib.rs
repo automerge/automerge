09c8f1ff7691552c531ed20bd39241756a7e9b57
Orion Henry <orion.henry@gmail.com>
2020-02-13 19:31:09 -0500

    migrate to serde-wasm-bindgen with result/error handling

--- automerge-backend-wasm/src/lib.rs
+++ automerge-backend-wasm/src/lib.rs
@@ -1,13 +1,12 @@
 extern crate automerge_backend;
-extern crate serde_json;
+extern crate serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
 use js_sys::Array;
+use serde_wasm_bindgen::{from_value,to_value};
 use automerge_backend::{Backend, ActorID, Change, Clock};
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -24,61 +23,61 @@ extern "C" {
 pub struct State { backend: Backend }
 
 #[wasm_bindgen(js_name = applyChange)]
-pub fn apply_changes(mut state: State, changes: JsValue) -> Array {
-  let c: Vec<Change> = changes.into_serde().unwrap();
+pub fn apply_changes(mut state: State, changes: JsValue) -> Result<Array,JsValue> {
+  let c: Vec<Change> = from_value(changes)?;
   let patch = state.backend.apply_changes(c);
   let ret = Array::new();
   ret.push(&state.into());
-  ret.push(&JsValue::from_serde(&patch).ok().into());
-  ret
+  ret.push(&to_value(&patch)?);
+  Ok(ret)
 }
 
 #[wasm_bindgen(js_name = applyLocalChange)]
-pub fn apply_local_change(mut state: State, change: JsValue) -> Array {
-  let c: Change = change.into_serde().unwrap();
+pub fn apply_local_change(mut state: State, change: JsValue) -> Result<Array,JsValue> {
+  let c: Change = from_value(change)?;
   let patch = state.backend.apply_local_change(c);
   let ret = Array::new();
   ret.push(&state.into());
-  ret.push(&JsValue::from_serde(&patch).ok().into());
-  ret
+  ret.push(&to_value(&patch)?);
+  Ok(ret)
 }
 
 #[wasm_bindgen(js_name = getPatch)]
-pub fn get_patch(state: &State) -> JsValue {
+pub fn get_patch(state: &State) -> Result<JsValue,JsValue> {
   let patch = state.backend.get_patch();
-  JsValue::from_serde(&patch).ok().into()
+  Ok(to_value(&patch)?)
 }
 
 #[wasm_bindgen(js_name = getChanges)]
-pub fn get_changes(state: &State) -> JsValue {
+pub fn get_changes(state: &State) -> Result<JsValue,JsValue> {
   let changes = state.backend.get_changes();
-  JsValue::from_serde(&changes).ok().into()
+  Ok(to_value(&changes)?)
 }
 
 #[wasm_bindgen(js_name = getChangesForActorId)]
-pub fn get_changes_for_actorid(state: &State, actorid: JsValue) -> JsValue {
-  let a: ActorID = actorid.into_serde().unwrap();
+pub fn get_changes_for_actorid(state: &State, actorid: JsValue) -> Result<JsValue,JsValue> {
+  let a: ActorID = from_value(actorid)?;
   let changes = state.backend.get_changes_for_actor_id(a);
-  JsValue::from_serde(&changes).ok().into()
+  Ok(to_value(&changes)?)
 }
 
 #[wasm_bindgen(js_name = getMissingChanges)]
-pub fn get_missing_changes(state: &State, clock: JsValue) -> JsValue {
-  let c: Clock = clock.into_serde().unwrap();
+pub fn get_missing_changes(state: &State, clock: JsValue) -> Result<JsValue,JsValue> {
+  let c: Clock = from_value(clock)?;
   let changes = state.backend.get_missing_changes(c);
-  JsValue::from_serde(&changes).ok().into()
+  Ok(to_value(&changes)?)
 }
 
 #[wasm_bindgen(js_name = getMissingDeps)]
-pub fn get_missing_deps(state: &State) -> JsValue {
+pub fn get_missing_deps(state: &State) -> Result<JsValue,JsValue> {
   let clock = state.backend.get_missing_deps();
-  JsValue::from_serde(&clock).ok().into()
+  Ok(to_value(&clock)?)
 }
 
 #[wasm_bindgen]
-pub fn merge(state: &mut State, remote: State) -> JsValue {
+pub fn merge(state: &mut State, remote: State) -> Result<JsValue,JsValue> {
   let patch = state.backend.merge(&remote.backend);
-  JsValue::from_serde(&patch).ok().into()
+  Ok(to_value(&patch)?)
 }
 
 #[wasm_bindgen]
