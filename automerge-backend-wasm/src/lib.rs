43aab45e133754592344e21687ee9ab6c08162e0
Orion Henry <orion.henry@gmail.com>
2020-02-13 19:16:36 -0500

    remove refcell and clone

--- automerge-backend-wasm/src/lib.rs
+++ automerge-backend-wasm/src/lib.rs
@@ -1,7 +1,5 @@
 extern crate automerge_backend;
 extern crate serde_json;
-use std::cell::RefCell;
-use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use js_sys::Array;
 use automerge_backend::{Backend, ActorID, Change, Clock};
@@ -23,71 +21,69 @@ extern "C" {
 // We need a wrapper object to attach the wasm-bindgen on
 #[wasm_bindgen]
 #[derive(PartialEq,Debug, Clone)]
-pub struct State { backend: Rc<RefCell<Backend>> }
+pub struct State { backend: Backend }
 
 #[wasm_bindgen(js_name = applyChange)]
-pub fn apply_changes(state: &State, changes: JsValue) -> Array {
+pub fn apply_changes(mut state: State, changes: JsValue) -> Array {
   let c: Vec<Change> = changes.into_serde().unwrap();
-  let new_state = state.clone();
-  let patch = new_state.backend.borrow_mut().apply_changes(c);
+  let patch = state.backend.apply_changes(c);
   let ret = Array::new();
-  ret.push(&new_state.into());
+  ret.push(&state.into());
   ret.push(&JsValue::from_serde(&patch).ok().into());
   ret
 }
 
 #[wasm_bindgen(js_name = applyLocalChange)]
-pub fn apply_local_change(state: &State, change: JsValue) -> Array {
+pub fn apply_local_change(mut state: State, change: JsValue) -> Array {
   let c: Change = change.into_serde().unwrap();
-  let new_state = state.clone();
-  let patch = new_state.backend.borrow_mut().apply_local_change(c);
+  let patch = state.backend.apply_local_change(c);
   let ret = Array::new();
-  ret.push(&new_state.into());
+  ret.push(&state.into());
   ret.push(&JsValue::from_serde(&patch).ok().into());
   ret
 }
 
 #[wasm_bindgen(js_name = getPatch)]
 pub fn get_patch(state: &State) -> JsValue {
-  let patch = state.backend.borrow().get_patch();
+  let patch = state.backend.get_patch();
   JsValue::from_serde(&patch).ok().into()
 }
 
 #[wasm_bindgen(js_name = getChanges)]
 pub fn get_changes(state: &State) -> JsValue {
-  let changes = state.backend.borrow().get_changes();
+  let changes = state.backend.get_changes();
   JsValue::from_serde(&changes).ok().into()
 }
 
 #[wasm_bindgen(js_name = getChangesForActorId)]
 pub fn get_changes_for_actorid(state: &State, actorid: JsValue) -> JsValue {
   let a: ActorID = actorid.into_serde().unwrap();
-  let changes = state.backend.borrow().get_changes_for_actor_id(a);
+  let changes = state.backend.get_changes_for_actor_id(a);
   JsValue::from_serde(&changes).ok().into()
 }
 
 #[wasm_bindgen(js_name = getMissingChanges)]
 pub fn get_missing_changes(state: &State, clock: JsValue) -> JsValue {
   let c: Clock = clock.into_serde().unwrap();
-  let changes = state.backend.borrow().get_missing_changes(c);
+  let changes = state.backend.get_missing_changes(c);
   JsValue::from_serde(&changes).ok().into()
 }
 
 #[wasm_bindgen(js_name = getMissingDeps)]
 pub fn get_missing_deps(state: &State) -> JsValue {
-  let clock = state.backend.borrow().get_missing_deps();
+  let clock = state.backend.get_missing_deps();
   JsValue::from_serde(&clock).ok().into()
 }
 
 #[wasm_bindgen]
 pub fn merge(state: &mut State, remote: State) -> JsValue {
-  let patch = state.backend.borrow_mut().merge(&remote.backend.borrow());
+  let patch = state.backend.merge(&remote.backend);
   JsValue::from_serde(&patch).ok().into()
 }
 
 #[wasm_bindgen]
 pub fn init() -> State {
-  State { backend: Rc::new(RefCell::new(Backend::init())) }
+  State { backend: Backend::init() }
 }
 
 #[wasm_bindgen(start)]
