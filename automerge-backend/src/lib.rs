cdbb98e0805a407581893251ee9d9ae72121de71
Alex Good <alex@memoryandthought.me>
2020-02-15 11:25:45 +0000

    Move patch into it's own module

--- automerge-backend/src/lib.rs
+++ automerge-backend/src/lib.rs
@@ -5,9 +5,9 @@ mod concurrent_operations;
 mod operation_with_metadata;
 mod object_store;
 mod op_set;
+mod patch;
 mod patch_serialization;
 mod protocol;
-use serde::Serialize;
 
 pub use crate::protocol::{
     ActorID, Change, Clock, DataType, Key, ObjectID, Operation, PrimitiveValue, ElementID
@@ -18,76 +18,7 @@ pub use concurrent_operations::ConcurrentOperations;
 pub use value::Value;
 pub use op_set::{OpSet, list_ops_in_order};
 pub use error::AutomergeError;
-pub use operation_with_metadata::OperationWithMetadata;
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum ElementValue {
-    Primitive(PrimitiveValue),
-    Link(ObjectID),
-}
-
-#[derive(Debug, PartialEq, Clone, Serialize)]
-pub enum SequenceType {
-    #[serde(rename = "list")]
-    List,
-    #[serde(rename = "text")]
-    Text,
-}
-
-#[derive(Debug, PartialEq, Clone, Serialize)]
-pub enum MapType {
-    #[serde(rename = "map")]
-    Map,
-    #[serde(rename = "table")]
-    Table,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum DiffAction {
-    CreateMap(ObjectID, MapType),
-    CreateList(ObjectID, SequenceType),
-    MaxElem(ObjectID, u32, SequenceType),
-    RemoveMapKey(ObjectID, MapType, Key),
-    SetMapKey(ObjectID, MapType, Key, ElementValue, Option<DataType>),
-    RemoveSequenceElement(ObjectID, SequenceType, u32),
-    InsertSequenceElement(ObjectID, SequenceType, u32, ElementValue, Option<DataType>),
-    SetSequenceElement(ObjectID, SequenceType, u32, ElementValue, Option<DataType>),
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct Conflict {
-    pub actor: ActorID,
-    pub value: ElementValue,
-    pub datatype: Option<DataType>,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct Diff {
-    pub action: DiffAction,
-    pub conflicts: Vec<Conflict>,
-}
-
-#[derive(Serialize, Debug, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct Patch {
-    pub can_undo: bool,
-    pub can_redo: bool,
-    pub clock: Clock,
-    pub deps: Clock,
-    pub diffs: Vec<Diff>,
-}
-
-impl Patch {
-    fn empty() -> Patch {
-        Patch {
-            can_undo: false,
-            can_redo: false,
-            clock: Clock::empty(),
-            deps: Clock::empty(),
-            diffs: Vec::new(),
-        }
-    }
-}
+pub use operation_with_metadata::OperationWithMetadata; pub use patch::{Patch, Diff, DiffAction, Conflict, MapType, SequenceType, ElementValue};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Backend {
---
+++ automerge-backend/src/patch.rs
@@ -0,0 +1,72 @@
+use serde::Serialize;
+use crate::{ObjectID, PrimitiveValue, DataType, ActorID, Key, Clock};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum ElementValue {
+    Primitive(PrimitiveValue),
+    Link(ObjectID),
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize)]
+pub enum SequenceType {
+    #[serde(rename = "list")]
+    List,
+    #[serde(rename = "text")]
+    Text,
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize)]
+pub enum MapType {
+    #[serde(rename = "map")]
+    Map,
+    #[serde(rename = "table")]
+    Table,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DiffAction {
+    CreateMap(ObjectID, MapType),
+    CreateList(ObjectID, SequenceType),
+    MaxElem(ObjectID, u32, SequenceType),
+    RemoveMapKey(ObjectID, MapType, Key),
+    SetMapKey(ObjectID, MapType, Key, ElementValue, Option<DataType>),
+    RemoveSequenceElement(ObjectID, SequenceType, u32),
+    InsertSequenceElement(ObjectID, SequenceType, u32, ElementValue, Option<DataType>),
+    SetSequenceElement(ObjectID, SequenceType, u32, ElementValue, Option<DataType>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Conflict {
+    pub actor: ActorID,
+    pub value: ElementValue,
+    pub datatype: Option<DataType>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Diff {
+    pub action: DiffAction,
+    pub conflicts: Vec<Conflict>,
+}
+
+#[derive(Serialize, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Patch {
+    pub can_undo: bool,
+    pub can_redo: bool,
+    pub clock: Clock,
+    pub deps: Clock,
+    pub diffs: Vec<Diff>,
+}
+
+impl Patch {
+    pub fn empty() -> Patch {
+        Patch {
+            can_undo: false,
+            can_redo: false,
+            clock: Clock::empty(),
+            deps: Clock::empty(),
+            diffs: Vec::new(),
+        }
+    }
+}
+
