de3d17dce9aad662340aa7ea2985d65375943833
Scott <scott@scotttrinh.com>
2020-05-18 21:52:16 -0400

    cli: Use StructOpt to build basic CLI opts struct

--- automerge-cli/src/main.rs
+++ automerge-cli/src/main.rs
@@ -1,25 +1,24 @@
-use clap::Clap;
-use std::{fs::File, io, io::Read};
+use std::path::Path;
+use structopt::StructOpt;
 
-#[derive(Debug, Clap)]
+mod error;
+mod export;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Automerge CLI")]
 struct Opts {
-    changes_file: String,
+    #[structopt(subcommand)]
+    cmd: Command,
 }
 
-fn main() -> io::Result<()> {
-    let opts = Opts::parse();
-    let mut input_data = Vec::new();
-    let mut input_file = File::open(opts.changes_file)?;
-    input_file.read_to_end(&mut input_data)?;
+#[derive(Debug, StructOpt)]
+enum Command {
+    Export { changes_file: String },
+}
 
-    let mut backend = automerge_backend::Backend::init();
-    backend.load_changes_binary(vec![input_data]).unwrap();
-    let patch = backend.get_patch().unwrap();
-    let mut frontend = automerge_frontend::Frontend::new();
-    frontend.apply_patch(patch).unwrap();
-    println!(
-        "{}",
-        serde_json::to_string_pretty(&frontend.state().to_json()).unwrap()
-    );
-    Ok(())
+fn main() -> Result<(), error::AutomergeCliError> {
+    let opts = Opts::from_args();
+    match opts.cmd {
+        Command::Export { changes_file } => export::export_json(Path::new(&changes_file)),
+    }
 }
