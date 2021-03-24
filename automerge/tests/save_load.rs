19f079f58a9e9ab0b334d31cea727f12d498b82d
Andrew Jeffery <dev@jeffas.io>
2021-03-24 13:58:17 +0000

    Add test to check save and load works on failing input
    We loop 100 times due to it being a flaky test, likely due to an
    iteration order issue in hashmaps.

---
+++ automerge/tests/save_load.rs
@@ -0,0 +1,182 @@
+use automerge::Backend;
+use automerge::Frontend;
+use automerge::LocalChange;
+use automerge::MapType;
+use automerge::Path;
+use automerge::Value;
+use automerge::{InvalidChangeRequest, Primitive};
+use pretty_assertions::assert_eq;
+
+#[test]
+fn missing_object_error() {
+    let mut change1s = Vec::new();
+    let mut change2s = Vec::new();
+
+    let actor_id = uuid::Uuid::new_v4();
+
+    for _ in 0..100 {
+        let changes1 = vec![LocalChange::set(
+            Path::root(),
+            Value::Map(
+                vec![
+                    (
+                        "\u{0}\u{0}".to_owned(),
+                        Value::Sequence(vec![
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                            Value::Primitive(Primitive::Boolean(false)),
+                            Value::Primitive(Primitive::Timestamp(0)),
+                            Value::Primitive(Primitive::Int(0)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Timestamp(0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Boolean(false)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                        ]),
+                    ),
+                    (
+                        "\u{2}".to_owned(),
+                        Value::Sequence(vec![
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                        ]),
+                    ),
+                    (
+                        "\u{0}".to_owned(),
+                        Value::Sequence(vec![
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Timestamp(0)),
+                            Value::Primitive(Primitive::Int(0)),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::F32(0.0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Timestamp(0)),
+                            Value::Primitive(Primitive::Timestamp(0)),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::F32(0.0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                        ]),
+                    ),
+                    (
+                        "".to_owned(),
+                        Value::Sequence(vec![
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::Int(0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::F32(0.0)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Uint(0)),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Timestamp(0)),
+                            Value::Primitive(Primitive::Str("".to_owned())),
+                            Value::Primitive(Primitive::Boolean(false)),
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Int(0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Null),
+                            Value::Primitive(Primitive::F64(0.0)),
+                            Value::Primitive(Primitive::Counter(0)),
+                            Value::Primitive(Primitive::Boolean(false)),
+                        ]),
+                    ),
+                    (
+                        "\u{1}".to_owned(),
+                        Value::Map(
+                            vec![("".to_owned(), Value::Primitive(Primitive::F64(0.0)))]
+                                .into_iter()
+                                .collect(),
+                            MapType::Table,
+                        ),
+                    ),
+                ]
+                .into_iter()
+                .collect(),
+                MapType::Map,
+            ),
+        )];
+        let changes2 = vec![
+            LocalChange::delete(Path::root().key("\u{0}\u{0}")),
+            LocalChange::delete(Path::root().key("\u{2}")),
+            LocalChange::delete(Path::root().key("\u{0}")),
+            LocalChange::delete(Path::root().key("")),
+            LocalChange::delete(Path::root().key("\u{1}")),
+        ];
+
+        let mut backend = Backend::init();
+        let mut frontend = Frontend::new_with_timestamper_and_actor_id(Box::new(|| None), actor_id);
+        let patch = backend.get_patch().unwrap();
+        frontend.apply_patch(patch).unwrap();
+
+        let c = frontend
+            .change::<_, InvalidChangeRequest>(None, |d| {
+                for change in &changes1 {
+                    d.add_change(change.clone())?
+                }
+                Ok(())
+            })
+            .unwrap();
+        if let Some(change) = c {
+            change1s.push(change.clone());
+            backend.apply_local_change(change).unwrap();
+        }
+
+        let backend_bytes = backend.save().unwrap();
+        println!("{:?}", backend_bytes);
+
+        let backend = Backend::load(backend_bytes);
+        if change1s.len() >= 2 {
+            assert_eq!(change1s[change1s.len() - 2], change1s[change1s.len() - 1])
+        }
+        match backend {
+            Err(e) => {
+                panic!("failed loading backend: {:?}", e)
+            }
+            Ok(mut backend) => {
+                let mut frontend =
+                    Frontend::new_with_timestamper_and_actor_id(Box::new(|| None), actor_id);
+                let patch = backend.get_patch().unwrap();
+                frontend.apply_patch(patch).unwrap();
+
+                let c = frontend
+                    .change::<_, InvalidChangeRequest>(None, |d| {
+                        for change in &changes2 {
+                            d.add_change(change.clone())?
+                        }
+                        Ok(())
+                    })
+                    .unwrap();
+                if let Some(change) = c {
+                    change2s.push(change.clone());
+                    backend.apply_local_change(change).unwrap();
+                }
+            }
+        }
+        if change2s.len() >= 2 {
+            assert_eq!(change2s[change2s.len() - 2], change2s[change2s.len() - 1])
+        }
+    }
+}
