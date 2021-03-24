a1686ee88dd6763818bdca9f6e02eb4d4ad838a5
Andrew Jeffery <dev@jeffas.io>
2021-03-24 17:35:24 +0000

    Update flake-utils input

--- flake.nix
+++ flake.nix
@@ -3,22 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs
-          {
-            overlays = [ rust-overlay.overlay ];
-            system = system;
-          };
-        rust = pkgs.rust-bin.nightly.latest.rust;
-        cargoNix = pkgs.callPackage ./Cargo.nix {
-          release = false;
+        pkgs = import nixpkgs {
+          overlays = [ rust-overlay.overlay ];
+          inherit system;
         };
+        rust = pkgs.rust-bin.nightly.latest.rust;
+        cargoNix = pkgs.callPackage ./Cargo.nix { };
       in
       {
         packages = {
