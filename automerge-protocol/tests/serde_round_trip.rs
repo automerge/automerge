61cd2decd6db16efaa791a13b7602d0e34aa838d
Vedant Roy <vroy101@gmail.com>
2021-06-01 15:34:51 -0400

    Properly update field count

--- automerge-protocol/tests/serde_round_trip.rs
+++ automerge-protocol/tests/serde_round_trip.rs
@@ -1,6 +1,8 @@
 extern crate automerge_protocol as amp;
+
 use maplit::hashmap;
 
+// This was not caught in the proptests
 #[test]
 fn test_msgpack_roundtrip_diff() {
     let actor = amp::ActorId::from_bytes("bd1850df21004038a8141a98473ff142".as_bytes());
