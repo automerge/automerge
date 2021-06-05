0f563da97f211e8d4e49bc3b22a4fffe9c87fe13
Andrew Jeffery <dev@jeffas.io>
2021-06-05 14:32:06 +0100

    Remove value_at_path
    This has the same functionality as get_value and we only need one.

--- automerge-frontend/src/lib.rs
+++ automerge-frontend/src/lib.rs
@@ -465,16 +465,9 @@ impl Frontend {
             .map(|o| o.values())
     }
 
+    /// Returns the value given by path, if it exists
     pub fn get_value(&self, path: &Path) -> Option<Value> {
         self.state.as_ref().and_then(|s| s.get_value(path))
-    }
-
-    /// Returns the value given by path, if it exists
-    pub fn value_at_path(&self, path: &Path) -> Option<Value> {
-        self.state
-            .as_ref()
-            .and_then(|s| s.resolve_path(&path))
-            .map(|o| o.default_value())
     }
 }
 
