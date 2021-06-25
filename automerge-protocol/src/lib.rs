a2e7b33b635e752e3d1ffd937e86742e123b6531
Vedant Roy <vroy101@gmail.com>
2021-06-25 11:58:30 -0400

    Address review comments

--- automerge-protocol/src/lib.rs
+++ automerge-protocol/src/lib.rs
@@ -332,39 +332,6 @@ impl ScalarValues {
     }
 }
 
-impl TryFrom<Vec<ScalarValue>> for ScalarValues {
-    type Error = InvalidScalarValues;
-    fn try_from(old_values: Vec<ScalarValue>) -> Result<Self, Self::Error> {
-        let mut values: Option<ScalarValues> = None;
-        for value in old_values.into_iter() {
-            if let Some(ref mut xs) = values {
-                if let Some(new_kind) = xs.append(value) {
-                    return Err(InvalidScalarValues::UnexpectedKind(xs.kind, new_kind));
-                }
-            } else {
-                values = Some(value.into());
-            }
-        }
-        values.ok_or(InvalidScalarValues::Empty)
-    }
-}
-
-impl From<ScalarValue> for ScalarValues {
-    fn from(value: ScalarValue) -> Self {
-        let kind = ScalarValueKind::from(&value);
-        Self {
-            vec: vec![value],
-            kind,
-        }
-    }
-}
-
-impl fmt::Display for ScalarValueKind {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
 #[derive(Serialize, PartialEq, Debug, Clone, EnumDiscriminants)]
 #[strum_discriminants(name(ScalarValueKind))]
 #[serde(untagged)]
---
+++ automerge-protocol/src/utility_impls/scalar_values.rs
@@ -0,0 +1,30 @@
+use std::convert::TryFrom;
+
+use crate::{InvalidScalarValues, ScalarValue, ScalarValueKind, ScalarValues};
+
+impl TryFrom<Vec<ScalarValue>> for ScalarValues {
+    type Error = InvalidScalarValues;
+    fn try_from(old_values: Vec<ScalarValue>) -> Result<Self, Self::Error> {
+        let mut values: Option<ScalarValues> = None;
+        for value in old_values.into_iter() {
+            if let Some(ref mut xs) = values {
+                if let Some(new_kind) = xs.append(value) {
+                    return Err(InvalidScalarValues::UnexpectedKind(xs.kind, new_kind));
+                }
+            } else {
+                values = Some(value.into());
+            }
+        }
+        values.ok_or(InvalidScalarValues::Empty)
+    }
+}
+
+impl From<ScalarValue> for ScalarValues {
+    fn from(value: ScalarValue) -> Self {
+        let kind = ScalarValueKind::from(&value);
+        Self {
+            vec: vec![value],
+            kind,
+        }
+    }
+}
