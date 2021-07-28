641c467e256d4a1eee1ecac56965c0d6249ceba6
Andrew Jeffery <dev@jeffas.io>
2021-07-28 15:39:11 +0100

    StateTreeSortedMap std -> stsm

--- automerge-frontend/src/value_ref/sorted_map.rs
+++ automerge-frontend/src/value_ref/sorted_map.rs
@@ -6,46 +6,46 @@ use crate::{state_tree::StateTreeSortedMap, value_ref::ValueRef, Value};
 
 #[derive(Clone, Debug)]
 pub struct SortedMapRef<'a> {
-    stm: &'a StateTreeSortedMap,
+    stsm: &'a StateTreeSortedMap,
 }
 
 impl<'a> SortedMapRef<'a> {
     pub(crate) fn new(stm: &'a StateTreeSortedMap) -> Self {
-        Self { stm }
+        Self { stsm: stm }
     }
 
     pub fn contains_key(&self, key: &str) -> bool {
-        self.stm.props.contains_key(key)
+        self.stsm.props.contains_key(key)
     }
 
     pub fn len(&self) -> usize {
-        self.stm.props.len()
+        self.stsm.props.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.stm.props.is_empty()
+        self.stsm.props.is_empty()
     }
 
     pub fn get(&self, key: &str) -> Option<ValueRef<'a>> {
-        self.stm
+        self.stsm
             .props
             .get(key)
             .map(|mv| ValueRef::new(mv.default_statetree_value()))
     }
 
     pub fn keys(&self) -> impl Iterator<Item = &SmolStr> {
-        self.stm.props.keys()
+        self.stsm.props.keys()
     }
 
     pub fn values(&self) -> impl Iterator<Item = ValueRef<'a>> {
-        self.stm
+        self.stsm
             .props
             .values()
             .map(|v| ValueRef::new(v.default_statetree_value()))
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (&SmolStr, ValueRef<'a>)> {
-        self.stm
+        self.stsm
             .props
             .iter()
             .map(|(k, v)| (k, ValueRef::new(v.default_statetree_value())))
@@ -57,7 +57,7 @@ impl<'a> SortedMapRef<'a> {
         R: RangeBounds<T>,
         SmolStr: Borrow<T>,
     {
-        self.stm
+        self.stsm
             .props
             .range(range)
             .map(|(k, v)| (k, ValueRef::new(v.default_statetree_value())))
@@ -65,7 +65,7 @@ impl<'a> SortedMapRef<'a> {
 
     pub fn value(&self) -> Value {
         let mut m = BTreeMap::new();
-        for (k, v) in &self.stm.props {
+        for (k, v) in &self.stsm.props {
             m.insert(k.clone(), v.default_value());
         }
         Value::SortedMap(m)
