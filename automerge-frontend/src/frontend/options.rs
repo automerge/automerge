e4b84aaf8e6dd46b182f3ad15a896d1053742d52
Andrew Jeffery <dev@jeffas.io>
2021-07-28 18:02:23 +0100

    Document options some more

--- automerge-frontend/src/frontend/options.rs
+++ automerge-frontend/src/frontend/options.rs
@@ -22,31 +22,47 @@ pub struct Options {
 }
 
 impl Options {
+    /// Set the schema.
     pub fn set_schema<S: Into<RootSchema>>(&mut self, schema: S) -> &mut Self {
         self.schema = Some(schema.into());
         self
     }
 
+    /// Set the schema.
     pub fn with_schema<S: Into<RootSchema>>(mut self, schema: S) -> Self {
         self.schema = Some(schema.into());
         self
     }
 
+    /// Set the actor id.
     pub fn set_actor_id<A: Into<ActorId>>(&mut self, actor_id: A) -> &mut Self {
         self.actor_id = actor_id.into();
         self
     }
 
+    /// Set the actor id.
     pub fn with_actor_id<A: Into<ActorId>>(mut self, actor_id: A) -> Self {
         self.actor_id = actor_id.into();
         self
     }
 
+    /// Set the timestamper function.
+    ///
+    /// The timestamper should produce values relative to the [Unix Epoch](https://en.wikipedia.org/wiki/Unix_time).
+    ///
+    /// For current discussion on the resolution provided see [this issue](https://github.com/automerge/automerge/issues/357).
+    /// Do note, however, that the default timestamper [`system_time`] uses millisecond resolution.
     pub fn set_timestamper(&mut self, timestamper: fn() -> Option<i64>) -> &mut Self {
         self.timestamper = timestamper;
         self
     }
 
+    /// Set the timestamper function.
+    ///
+    /// The timestamper should produce values relative to the [Unix Epoch](https://en.wikipedia.org/wiki/Unix_time).
+    ///
+    /// For current discussion on the resolution provided see [this issue](https://github.com/automerge/automerge/issues/357).
+    /// Do note, however, that the default timestamper [`system_time`] uses millisecond resolution.
     pub fn with_timestamper(mut self, timestamper: fn() -> Option<i64>) -> Self {
         self.timestamper = timestamper;
         self
@@ -64,6 +80,8 @@ impl Default for Options {
 }
 
 /// Use the default timestamp since the Unix Epoch.
+///
+/// This produces millisecond resolution.
 pub fn system_time() -> Option<i64> {
     std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
