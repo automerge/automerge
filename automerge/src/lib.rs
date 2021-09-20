899e4584b2e18548a88549e1603fcbb2ad4172d5
Alex Good <alex@memoryandthought.me>
2021-09-20 15:39:46 +0100

    Re-export errors

--- automerge/src/lib.rs
+++ automerge/src/lib.rs
@@ -1,5 +1,5 @@
-pub use automerge_backend::{Backend, Change};
+pub use automerge_backend::{Backend, Change, AutomergeError as BackendError};
 pub use automerge_frontend::{
-    value_ref, Frontend, InvalidChangeRequest, LocalChange, MutableDocument, Path, Primitive, Value,
+    value_ref, Frontend, InvalidChangeRequest, LocalChange, MutableDocument, Path, Primitive, Value, AutomergeFrontendError as FrontendError, InvalidPatch
 };
 pub use automerge_protocol::{MapType, ObjType, ScalarValue, SequenceType};
