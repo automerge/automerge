22342bbbc1dfcd75f47f98d196a807d45c81855e
Alex Good <alex@memoryandthought.me>
2021-09-20 15:52:10 +0100

    fmt

--- automerge/src/lib.rs
+++ automerge/src/lib.rs
@@ -1,5 +1,6 @@
-pub use automerge_backend::{Backend, Change, AutomergeError as BackendError};
+pub use automerge_backend::{AutomergeError as BackendError, Backend, Change};
 pub use automerge_frontend::{
-    value_ref, Frontend, InvalidChangeRequest, LocalChange, MutableDocument, Path, Primitive, Value, AutomergeFrontendError as FrontendError, InvalidPatch
+    value_ref, AutomergeFrontendError as FrontendError, Frontend, InvalidChangeRequest,
+    InvalidPatch, LocalChange, MutableDocument, Path, Primitive, Value,
 };
 pub use automerge_protocol::{MapType, ObjType, ScalarValue, SequenceType};
