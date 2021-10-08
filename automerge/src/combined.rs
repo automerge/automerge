5369aa8b6d293126442c288342e89b2ec5822434
Andrew Jeffery <dev@jeffas.io>
2021-10-08 18:23:43 +0100

    Add sync functionality and some basic tests

--- automerge/src/combined.rs
+++ automerge/src/combined.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use automerge_backend::{Backend, Change};
+use automerge_backend::{Backend, Change, SyncMessage, SyncState};
 use automerge_frontend::{value_ref::RootRef, Frontend, MutableDocument, Path, Value};
 use automerge_protocol as amp;
 use automerge_protocol::OpId;
@@ -100,5 +100,121 @@ impl Automerge {
         let mut frontend = Frontend::new();
         frontend.apply_patch(patch)?;
         Ok(Self { frontend, backend })
+    }
+
+    pub fn generate_sync_message(&self, sync_state: &mut SyncState) -> Option<SyncMessage> {
+        self.backend.generate_sync_message(sync_state)
+    }
+
+    pub fn receive_sync_message(
+        &mut self,
+        sync_state: &mut SyncState,
+        message: SyncMessage,
+    ) -> Result<(), AutomergeError> {
+        let patch = self.backend.receive_sync_message(sync_state, message)?;
+        if let Some(patch) = patch {
+            self.frontend.apply_patch(patch)?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use automerge_frontend::{LocalChange, Primitive};
+
+    use super::*;
+
+    #[test]
+    fn change() {
+        let mut a = Automerge::new();
+
+        let path = Path::root().key("a");
+        let value = Value::Primitive(Primitive::Str("test".into()));
+
+        let ((), _change) = a
+            .change(None, |doc| {
+                doc.add_change(LocalChange::set(path.clone(), value.clone()))
+            })
+            .unwrap();
+
+        let found_value = a.get_value(&path);
+        assert_eq!(found_value, Some(value));
+    }
+
+    #[test]
+    fn save_load() {
+        let mut a = Automerge::new();
+
+        let path = Path::root().key("a");
+        let value = Value::Primitive(Primitive::Str("test".into()));
+
+        let ((), _change) = a
+            .change(None, |doc| {
+                doc.add_change(LocalChange::set(path.clone(), value.clone()))
+            })
+            .unwrap();
+
+        let found_value = a.get_value(&path);
+        assert_eq!(found_value, Some(value));
+
+        let bytes = a.save().unwrap();
+        let b = Automerge::load(bytes).unwrap();
+
+        assert_eq!(b.get_value(&Path::root()), a.get_value(&Path::root()))
+    }
+
+    #[test]
+    fn foreign_change() {
+        let mut a = Automerge::new();
+
+        let path = Path::root().key("a");
+        let value = Value::Primitive(Primitive::Str("test".into()));
+
+        let ((), _change) = a
+            .change(None, |doc| {
+                doc.add_change(LocalChange::set(path.clone(), value.clone()))
+            })
+            .unwrap();
+
+        let found_value = a.get_value(&path);
+        assert_eq!(found_value, Some(value));
+
+        let mut b = Automerge::new();
+        b.apply_changes(a.get_changes(&[]).into_iter().cloned().collect())
+            .unwrap();
+
+        assert_eq!(b.get_value(&Path::root()), a.get_value(&Path::root()))
+    }
+
+    #[test]
+    fn sync() {
+        let mut a = Automerge::new();
+
+        let path = Path::root().key("a");
+        let value = Value::Primitive(Primitive::Str("test".into()));
+
+        let ((), _change) = a
+            .change(None, |doc| {
+                doc.add_change(LocalChange::set(path.clone(), value.clone()))
+            })
+            .unwrap();
+
+        let found_value = a.get_value(&path);
+        assert_eq!(found_value, Some(value));
+
+        let mut a_sync_state = SyncState::default();
+        let mut b_sync_state = SyncState::default();
+
+        let mut b = Automerge::new();
+
+        let msg = b.generate_sync_message(&mut a_sync_state).unwrap();
+        a.receive_sync_message(&mut b_sync_state, msg).unwrap();
+
+        let msg = a.generate_sync_message(&mut b_sync_state).unwrap();
+        b.receive_sync_message(&mut a_sync_state, msg).unwrap();
+
+        assert_eq!(b.get_value(&Path::root()), a.get_value(&Path::root()))
     }
 }
