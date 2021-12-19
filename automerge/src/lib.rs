6e5a061cfd8d4abd00e275c14949ffe0b323df06
Alex Good <alex@memoryandthought.me>
2021-12-19 20:18:06 +0000

    Add Automerge::visualise for debugging the optree
    The OpTree is a reasonably complicated data structure. This change adds
    a method on the OpTree which is then exposed through
    `Automerge::visualise` which prints the structure of the OpTree in
    graphviz format. Each node in the tree is printed as a simple table of
    operations with outgoing edges to tables representing the children of
    the node. This is behind a compile time flag (optree-visualisation)
    because it's probably not useful except when debugging.

--- automerge/src/lib.rs
+++ automerge/src/lib.rs
@@ -1014,6 +1014,13 @@ impl Automerge {
             );
         }
     }
+
+    #[cfg(feature = "optree-visualisation")]
+    pub fn visualise_optree(&self) -> String {
+        let mut out = Vec::new();
+        self.ops.visualise(&mut out).unwrap();
+        String::from_utf8_lossy(&out[..]).to_string()
+    }
 }
 
 #[derive(Debug, Clone)]
