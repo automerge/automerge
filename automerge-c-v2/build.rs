ba038e5aa5de1a40138196319ed3faf02d2741ab
Jason A. Kankiewicz <jason.kankiewicz@gmail.com>
2022-01-23 23:08:35 -0800

    Add CMake support to resolve #264

--- automerge-c-v2/build.rs
+++ automerge-c-v2/build.rs
@@ -4,7 +4,7 @@ use std::{env, path::PathBuf};
 
 fn main() {
     let crate_dir = PathBuf::from(
-        env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var is not defined"),
+        env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var is not defined")
     );
 
     let config = cbindgen::Config::from_file("cbindgen.toml")
@@ -12,5 +12,11 @@ fn main() {
 
     if let Ok(writer) = cbindgen::generate_with_config(&crate_dir, config) {
         writer.write_to_file(crate_dir.join("automerge.h"));
+
+        // Also write the generated header into the target directory when
+        // specified (necessary for an out-of-source build a la CMake).
+        if let Ok(target_dir) = env::var("CARGO_TARGET_DIR") {
+            writer.write_to_file(PathBuf::from(target_dir).join("automerge.h"));
+        }
     }
 }
