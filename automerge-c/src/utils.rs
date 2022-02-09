60835e6ae7361199e03225dd33a8aa9c4614a20e
Orion Henry <orion.henry@gmail.com>
2022-02-09 15:50:44 -0500

    rework to return a queriable result

--- automerge-c/src/utils.rs
+++ automerge-c/src/utils.rs
@@ -1,27 +1,13 @@
+use crate::{AMobj, AMresult, Datatype};
 use automerge as am;
-use libc::{c_long, c_ulong, c_double};
+use libc::{c_double, c_long, c_ulong};
 use std::{
     ffi::{c_void, CStr},
-    ops::{Deref, DerefMut},
+    ops::Deref,
     os::raw::c_char,
 };
-use crate::{ ObjId, Automerge, Prop, CError , Datatype };
 
-impl Deref for Automerge {
-    type Target = am::Automerge;
-
-    fn deref(&self) -> &Self::Target {
-        &self.handle
-    }
-}
-
-impl DerefMut for Automerge {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.handle
-    }
-}
-
-impl Deref for ObjId {
+impl Deref for AMobj {
     type Target = am::ObjId;
 
     fn deref(&self) -> &Self::Target {
@@ -29,21 +15,16 @@ impl Deref for ObjId {
     }
 }
 
-impl From<*const ObjId> for ObjId {
-    fn from(obj: *const ObjId) -> Self {
-        unsafe { obj.as_ref().cloned().unwrap_or(ObjId(am::ROOT)) }
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+impl From<*const AMobj> for AMobj {
+    fn from(obj: *const AMobj) -> Self {
+        unsafe { obj.as_ref().cloned().unwrap_or(AMobj(am::ROOT)) }
     }
 }
 
-impl From<Automerge> for *mut Automerge {
-    fn from(b: Automerge) -> Self {
+impl From<AMresult> for *mut AMresult {
+    fn from(b: AMresult) -> Self {
         Box::into_raw(Box::new(b))
-    }
-}
-
-impl From<*const c_char> for Prop {
-    fn from(c: *const c_char) -> Self {
-        unsafe { Prop(CStr::from_ptr(c).to_string_lossy().to_string().into()) }
     }
 }
 
@@ -67,61 +48,51 @@ impl From<&am::Value> for Datatype {
     }
 }
 
-impl From<Prop> for am::Prop {
-    fn from(p: Prop) -> Self {
-        p.0
-    }
-}
-
-impl From<am::AutomergeError> for CError {
-    fn from(e: am::AutomergeError) -> Self {
-      CError::AutomergeError(e)
-    }
-}
-
-pub (crate) fn import_value(value: *const c_void, datatype: Datatype) -> Result<am::Value, CError> {
-  unsafe {
-    match datatype {
-      Datatype::Str => {
-        let value : *const c_char = value.cast();
-        if !value.is_null() {
-          Some(CStr::from_ptr(value).to_string_lossy().to_string().into())
-        } else {
-          None
+pub(crate) fn import_value(
+    value: *const c_void,
+    datatype: Datatype,
+) -> Result<am::Value, AMresult> {
+    unsafe {
+        match datatype {
+            Datatype::Str => {
+                let value: *const c_char = value.cast();
+                if !value.is_null() {
+                    Some(CStr::from_ptr(value).to_string_lossy().to_string().into())
+                } else {
+                    None
+                }
+            }
+            Datatype::Boolean => value
+                .cast::<*const c_char>()
+                .as_ref()
+                .map(|v| am::Value::boolean(**v != 0)),
+            Datatype::Int => value
+                .cast::<*const c_long>()
+                .as_ref()
+                .map(|v| am::Value::int(**v)),
+            Datatype::Uint => value
+                .cast::<*const c_ulong>()
+                .as_ref()
+                .map(|v| am::Value::uint(**v)),
+            Datatype::F64 => value
+                .cast::<*const c_double>()
+                .as_ref()
+                .map(|v| am::Value::f64(**v)),
+            Datatype::Timestamp => value
+                .cast::<*const c_long>()
+                .as_ref()
+                .map(|v| am::Value::timestamp(**v)),
+            Datatype::Counter => value
+                .cast::<*const c_long>()
+                .as_ref()
+                .map(|v| am::Value::counter(**v)),
+            Datatype::Null => Some(am::Value::null()),
+            Datatype::Map => Some(am::Value::map()),
+            Datatype::List => Some(am::Value::list()),
+            Datatype::Text => Some(am::Value::text()),
+            Datatype::Table => Some(am::Value::table()),
+            _ => return Err(AMresult::Error("Invalid datatype".into())),
         }
-      },
-      Datatype::Boolean => {
-        value.cast::<*const c_char>().as_ref()
-          .map(|v| am::Value::boolean(**v != 0))
-      },
-      Datatype::Int => {
-        value.cast::<*const c_long>().as_ref()
-          .map(|v| am::Value::int(**v))
-      },
-      Datatype::Uint => {
-        value.cast::<*const c_ulong>().as_ref()
-          .map(|v| am::Value::uint(**v))
-      },
-      Datatype::F64 => {
-        value.cast::<*const c_double>().as_ref()
-          .map(|v| am::Value::f64(**v))
-      },
-      Datatype::Timestamp => {
-        value.cast::<*const c_long>().as_ref()
-          .map(|v| am::Value::timestamp(**v))
-      },
-      Datatype::Counter => {
-        value.cast::<*const c_long>().as_ref()
-          .map(|v| am::Value::counter(**v))
-      },
-      Datatype::Null =>Some(am::Value::null()),
-      Datatype::Map => Some(am::Value::map()),
-      Datatype::List => Some(am::Value::list()),
-      Datatype::Text => Some(am::Value::text()),
-      Datatype::Table => Some(am::Value::table()),
-      _ => {
-        return Err(CError::InvalidDatatype(datatype))
-      }
-    }.ok_or(CError::NullValue(datatype))
-  }
+        .ok_or_else(|| AMresult::Error("Null value".into()))
+    }
 }
