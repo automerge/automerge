b0b803eef8d07b5171b9ab736eae3e2f13d6158b
Orion Henry <orion.henry@gmail.com>
2022-02-22 11:30:42 -0500

    get simple test passing

--- automerge-c/automerge.c
+++ automerge-c/automerge.c
@@ -10,62 +10,24 @@ int main() {
   int datatype = 0;
   char buff[MAX_BUFF_SIZE];
   char obj[MAX_BUFF_SIZE];
+  AMresult * res = NULL;
 
   printf("begin\n");
 
-  Automerge * doc  = am_create();
+  AMdoc * doc  = AMcreate();
 
-  printf("am_set_actor_hex()...");
-  am_set_actor_hex(doc, "aabbcc");
+  printf("AMconfig()...");
+  AMconfig(doc, "actor", "aabbcc");
   printf("pass!\n");
 
-  printf("testing str...");
-
-  printf("am_map_set()...");
-  n = am_map_set(doc, AM_ROOT, "string", Datatype_Str, "hello world");
-  assert(n == 0);
-  printf("pass!\n");
-
-  printf("am_map_values()...");
-  n = am_map_values(doc, AM_ROOT, "string");
-  assert(n == 1);
-  printf("pass!\n");
-
-  printf("am_pop_value()...");
-  n = am_pop_value(doc, &datatype, buff, MAX_BUFF_SIZE);
-  assert(n == strlen("hello world") + 1);
-  assert(datatype == Datatype_Str);
-  assert(strcmp(buff, "hello world") == 0);
-  printf("pass!\n");
-
-  printf("testing list...");
-
-  printf("am_map_set()...");
-  n = am_map_set(doc, AM_ROOT, "list", Datatype_List, NULL);
-  assert(n == 1);
-  printf("pass!\n");
-
-  /*
-  printf("am_pop()...");
-  n = am_pop(doc, buff, MAX_BUFF_SIZE);
-  assert(n == 3);
-  printf("pass!\n");
-  */
-
-  printf("am_map_values()...");
-  n = am_map_values(doc, AM_ROOT, "list");
-  assert(n == 1);
-  printf("pass!\n");
-
-  printf("am_pop_value()...");
-  n = am_pop_value(doc, &datatype, buff, MAX_BUFF_SIZE);
-  assert(n == strlen("list") + 1);
-  assert(datatype == Datatype_List);
-  assert(strcmp(buff, "list") == 0);
-  printf("pass!\n");
-
-  printf("am_free()...");
-  am_free(doc);
+  printf("AMmapSet()...\n");
+  res = AMmapSet(doc, NULL, "string", AM_DATATYPE_STR, "hello world");
+  if (AMresultStatus(res) != AM_STATUS_COMMAND_OK)
+  {
+        printf("AMmapSet() failed: %s\n", AMerrorMessage(res));
+        return 1;
+  }
+  AMclear(res);
   printf("pass!\n");
 
   printf("end\n");
