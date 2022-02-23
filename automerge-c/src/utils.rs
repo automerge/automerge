7f650fb8e0e7173c72d192c60506e46abb41f92f
Jason Kankiewicz <jason.kankiewicz@gmail.com>
2022-02-23 02:14:06 -0800

    Added Doxygen documentation generation. Renamed `AMDatatype` to `AmDataType`. Reorganized the `AmDataType` tags. Renamed `AMfree()` to `AMdestroy()`. Renamed `AMclone()` to `AMdup()`.

--- automerge-c/src/utils.rs
+++ automerge-c/src/utils.rs
@@ -1,4 +1,4 @@
-use crate::{AMobj, AMresult, AmDatatype};
+use crate::{AMobj, AMresult, AmDataType};
 use automerge as am;
 use libc::{c_double, c_long, c_ulong};
 use std::{
@@ -28,33 +28,33 @@ impl From<AMresult> for *mut AMresult {
     }
 }
 
-impl From<&am::Value> for AmDatatype {
+impl From<&am::Value> for AmDataType {
     fn from(v: &am::Value) -> Self {
         match v {
-            am::Value::Scalar(am::ScalarValue::Str(_)) => AmDatatype::Str,
-            am::Value::Scalar(am::ScalarValue::Int(_)) => AmDatatype::Int,
-            am::Value::Scalar(am::ScalarValue::Uint(_)) => AmDatatype::Uint,
-            am::Value::Scalar(am::ScalarValue::F64(_)) => AmDatatype::F64,
-            am::Value::Scalar(am::ScalarValue::Boolean(_)) => AmDatatype::Boolean,
-            am::Value::Scalar(am::ScalarValue::Bytes(_)) => AmDatatype::Bytes,
-            am::Value::Scalar(am::ScalarValue::Counter(_)) => AmDatatype::Counter,
-            am::Value::Scalar(am::ScalarValue::Timestamp(_)) => AmDatatype::Timestamp,
-            am::Value::Scalar(am::ScalarValue::Null) => AmDatatype::Null,
-            am::Value::Object(am::ObjType::Map) => AmDatatype::Map,
-            am::Value::Object(am::ObjType::List) => AmDatatype::List,
-            am::Value::Object(am::ObjType::Table) => AmDatatype::Table,
-            am::Value::Object(am::ObjType::Text) => AmDatatype::Text,
+            am::Value::Scalar(am::ScalarValue::Str(_)) => AmDataType::Str,
+            am::Value::Scalar(am::ScalarValue::Int(_)) => AmDataType::Int,
+            am::Value::Scalar(am::ScalarValue::Uint(_)) => AmDataType::Uint,
+            am::Value::Scalar(am::ScalarValue::F64(_)) => AmDataType::F64,
+            am::Value::Scalar(am::ScalarValue::Boolean(_)) => AmDataType::Boolean,
+            am::Value::Scalar(am::ScalarValue::Bytes(_)) => AmDataType::Bytes,
+            am::Value::Scalar(am::ScalarValue::Counter(_)) => AmDataType::Counter,
+            am::Value::Scalar(am::ScalarValue::Timestamp(_)) => AmDataType::Timestamp,
+            am::Value::Scalar(am::ScalarValue::Null) => AmDataType::Null,
+            am::Value::Object(am::ObjType::Map) => AmDataType::Map,
+            am::Value::Object(am::ObjType::List) => AmDataType::List,
+            am::Value::Object(am::ObjType::Table) => AmDataType::Table,
+            am::Value::Object(am::ObjType::Text) => AmDataType::Text,
         }
     }
 }
 
 pub(crate) fn import_value(
     value: *const c_void,
-    datatype: AmDatatype,
+    data_type: AmDataType,
 ) -> Result<am::Value, AMresult> {
     unsafe {
-        match datatype {
-            AmDatatype::Str => {
+        match data_type {
+            AmDataType::Str => {
                 let value: *const c_char = value.cast();
                 if !value.is_null() {
                     Some(CStr::from_ptr(value).to_string_lossy().to_string().into())
@@ -62,36 +62,36 @@ pub(crate) fn import_value(
                     None
                 }
             }
-            AmDatatype::Boolean => value
+            AmDataType::Boolean => value
                 .cast::<*const c_char>()
                 .as_ref()
                 .map(|v| am::Value::boolean(**v != 0)),
-            AmDatatype::Int => value
+            AmDataType::Int => value
                 .cast::<*const c_long>()
                 .as_ref()
                 .map(|v| am::Value::int(**v)),
-            AmDatatype::Uint => value
+            AmDataType::Uint => value
                 .cast::<*const c_ulong>()
                 .as_ref()
                 .map(|v| am::Value::uint(**v)),
-            AmDatatype::F64 => value
+            AmDataType::F64 => value
                 .cast::<*const c_double>()
                 .as_ref()
                 .map(|v| am::Value::f64(**v)),
-            AmDatatype::Timestamp => value
+            AmDataType::Timestamp => value
                 .cast::<*const c_long>()
                 .as_ref()
                 .map(|v| am::Value::timestamp(**v)),
-            AmDatatype::Counter => value
+            AmDataType::Counter => value
                 .cast::<*const c_long>()
                 .as_ref()
                 .map(|v| am::Value::counter(**v)),
-            AmDatatype::Null => Some(am::Value::null()),
-            AmDatatype::Map => Some(am::Value::map()),
-            AmDatatype::List => Some(am::Value::list()),
-            AmDatatype::Text => Some(am::Value::text()),
-            AmDatatype::Table => Some(am::Value::table()),
-            _ => return Err(AMresult::err("Invalid datatype")),
+            AmDataType::Null => Some(am::Value::null()),
+            AmDataType::Map => Some(am::Value::map()),
+            AmDataType::List => Some(am::Value::list()),
+            AmDataType::Text => Some(am::Value::text()),
+            AmDataType::Table => Some(am::Value::table()),
+            _ => return Err(AMresult::err("Invalid data type")),
         }
         .ok_or_else(|| AMresult::err("Null value"))
     }
