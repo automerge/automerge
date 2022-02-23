27ded5402ba60d15d7c8b33da0a263f1149969e5
Jason Kankiewicz <jason.kankiewicz@gmail.com>
2022-02-23 02:14:06 -0800

    Added Doxygen documentation generation. Renamed `AMDatatype` to `AmDataType`. Reorganized the `AmDataType` tags. Renamed `AMfree()` to `AMdestroy()`. Renamed `AMclone()` to `AMdup()`.

--- automerge-c/automerge.c
+++ automerge-c/automerge.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include "automerge.h"
@@ -7,21 +8,21 @@
 
 int main() {
   int n = 0;
-  int datatype = 0;
+  int data_type = 0;
   char buff[MAX_BUFF_SIZE];
   char obj[MAX_BUFF_SIZE];
-  AMresult * res = NULL;
+  AMresult* res = NULL;
 
   printf("begin\n");
 
-  AMdoc * doc  = AMcreate();
+  AMdoc* doc  = AMcreate();
 
   printf("AMconfig()...");
   AMconfig(doc, "actor", "aabbcc");
   printf("pass!\n");
 
   printf("AMmapSet()...\n");
-  res = AMmapSet(doc, NULL, "string", AM_DATATYPE_STR, "hello world");
+  res = AMmapSet(doc, NULL, "string", AM_DATA_TYPE_STR, "hello world");
   if (AMresultStatus(res) != AM_STATUS_COMMAND_OK)
   {
         printf("AMmapSet() failed: %s\n", AMerrorMessage(res));
@@ -30,5 +31,6 @@ int main() {
   AMclear(res);
   printf("pass!\n");
 
+  AMdestroy(doc);
   printf("end\n");
 }
