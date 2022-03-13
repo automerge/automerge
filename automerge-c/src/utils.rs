335cd1c85f099c431843bc930dafde1bb1174eaf
Jason Kankiewicz <jason.kankiewicz@gmail.com>
2022-03-13 16:27:39 -0700

    Removed superfluous `AMobj` traits.

--- automerge-c/src/utils.rs
+++ automerge-c/src/utils.rs
@@ -1,21 +1,4 @@
-use crate::{AMobj, AMresult};
-use automerge as am;
-use std::ops::Deref;
-
-impl Deref for AMobj {
-    type Target = am::ObjId;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-impl From<*const AMobj> for AMobj {
-    fn from(obj: *const AMobj) -> Self {
-        unsafe { obj.as_ref().cloned().unwrap_or(AMobj(am::ROOT)) }
-    }
-}
+use crate::AMresult;
 
 impl From<AMresult> for *mut AMresult {
     fn from(b: AMresult) -> Self {
