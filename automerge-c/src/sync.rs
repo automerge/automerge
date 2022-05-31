d9bf29e8fdb0989d7f644d1a636147561f9a0046
Jason Kankiewicz <jason.kankiewicz@gmail.com>
2022-05-30 22:50:26 -0700

    Grouped `AMsyncMessage` and `AMsyncState` into separate source files.

--- automerge-c/src/sync.rs
+++ automerge-c/src/sync.rs
@@ -1,39 +1,7 @@
-use automerge as am;
+mod have;
+mod haves;
+mod message;
+mod state;
 
-/// \struct AMsyncMessage
-/// \brief A synchronization message for a peer.
-pub struct AMsyncMessage(am::sync::Message);
-
-impl AMsyncMessage {
-    pub fn new(message: am::sync::Message) -> Self {
-        Self(message)
-    }
-}
-
-impl AsRef<am::sync::Message> for AMsyncMessage {
-    fn as_ref(&self) -> &am::sync::Message {
-        &self.0
-    }
-}
-
-/// \struct AMsyncState
-/// \brief The state of synchronization with a peer.
-pub struct AMsyncState(am::sync::State);
-
-impl AMsyncState {
-    pub fn new(state: am::sync::State) -> Self {
-        Self(state)
-    }
-}
-
-impl AsMut<am::sync::State> for AMsyncState {
-    fn as_mut(&mut self) -> &mut am::sync::State {
-        &mut self.0
-    }
-}
-
-impl From<AMsyncState> for *mut AMsyncState {
-    fn from(b: AMsyncState) -> Self {
-        Box::into_raw(Box::new(b))
-    }
-}
+pub(crate) use message::{to_sync_message, AMsyncMessage};
+pub(crate) use state::AMsyncState;
