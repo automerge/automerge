fb0ea2c7a4789fe654b904f9bbd388acebaf7d47
Jason Kankiewicz <jason.kankiewicz@gmail.com>
2022-05-30 22:12:03 -0700

    Renamed `AMadvanceChangeHashes()` to `AMchangeHashesAdvance()`. Added `AMchangeHashesCmp()`. Renamed `AMnextChangeHash()` to `AMchangeHashesNext()`.

--- automerge-c/src/change_hashes.rs
+++ automerge-c/src/change_hashes.rs
@@ -1,21 +1,20 @@
 use automerge as am;
+use std::cmp::Ordering;
 use std::ffi::c_void;
 
-use crate::AMbyteSpan;
+use crate::byte_span::AMbyteSpan;
 
 /// \struct AMchangeHashes
-/// \brief A bidirectional iterator over a sequence of `AMbyteSpan` structs.
+/// \brief A bidirectional iterator over a sequence of change hashes.
 #[repr(C)]
 pub struct AMchangeHashes {
+    /// The length of the sequence.
     len: usize,
+    /// The offset from \p ptr, \p +offset -> forward direction,
+    /// \p -offset -> reverse direction.
     offset: isize,
+    /// A pointer to the first change hash or `NULL`.
     ptr: *const c_void,
-}
-
-impl AsRef<[am::ChangeHash]> for AMchangeHashes {
-    fn as_ref(&self) -> &[am::ChangeHash] {
-        unsafe { std::slice::from_raw_parts(self.ptr as *const am::ChangeHash, self.len) }
-    }
 }
 
 impl AMchangeHashes {
@@ -65,23 +64,128 @@ impl AMchangeHashes {
     }
 }
 
+impl AsRef<[am::ChangeHash]> for AMchangeHashes {
+    fn as_ref(&self) -> &[am::ChangeHash] {
+        unsafe { std::slice::from_raw_parts(self.ptr as *const am::ChangeHash, self.len) }
+    }
+}
+
+impl Default for AMchangeHashes {
+    fn default() -> Self {
+        Self {
+            len: 0,
+            offset: 0,
+            ptr: std::ptr::null(),
+        }
+    }
+}
+
 /// \memberof AMchangeHashes
 /// \brief Advances/rewinds an `AMchangeHashes` struct by at most \p |n|
 /// positions.
 ///
 /// \param[in] change_hashes A pointer to an `AMchangeHashes` struct.
 /// \param[in] n The direction (\p -n -> backward, \p +n -> forward) and maximum
-/// number of positions to advance/rewind.
+///              number of positions to advance/rewind.
 /// \pre \p change_hashes must be a valid address.
 /// \internal
 ///
 /// #Safety
 /// change_hashes must be a pointer to a valid AMchangeHashes
 #[no_mangle]
-pub unsafe extern "C" fn AMadvanceChangeHashes(change_hashes: *mut AMchangeHashes, n: isize) {
+pub unsafe extern "C" fn AMchangeHashesAdvance(change_hashes: *mut AMchangeHashes, n: isize) {
     if let Some(change_hashes) = change_hashes.as_mut() {
         change_hashes.advance(n);
     };
+}
+
+/// \memberof AMchangeHashes
+/// \brief Compares two change hash sequences.
+///
+/// \param[in] change_hashes1 A pointer to an `AMchangeHashes` struct.
+/// \param[in] change_hashes2 A pointer to an `AMchangeHashes` struct.
+/// \return `-1` if \p change_hashes1 `<` \p change_hashes2, `0` if
+///         \p change_hashes1 `==` \p change_hashes2 and `1` if
+///         \p change_hashes1 `>` \p change_hashes2.
+/// \pre \p change_hashes1 must be a valid address.
+/// \pre \p change_hashes2 must be a valid address.
+/// \internal
+///
+/// #Safety
+/// change_hashes1 must be a pointer to a valid AMchangeHashes
+/// change_hashes2 must be a pointer to a valid AMchangeHashes
+#[no_mangle]
+pub unsafe extern "C" fn AMchangeHashesCmp(
+    change_hashes1: *const AMchangeHashes,
+    change_hashes2: *const AMchangeHashes,
+) -> isize {
+    match (change_hashes1.as_ref(), change_hashes2.as_ref()) {
+        (Some(change_hashes1), Some(change_hashes2)) => {
+            match change_hashes1.as_ref().cmp(change_hashes2.as_ref()) {
+                Ordering::Less => -1,
+                Ordering::Equal => 0,
+                Ordering::Greater => 1,
+            }
+        }
+        (None, Some(_)) => -1,
+        (Some(_), None) => 1,
+        (None, None) => 0,
+    }
+}
+
+/// \memberof AMchangeHashes
+/// \brief Gets the `AMbyteSpan` struct at the current position of an
+/// `AMchangeHashes`struct and then advances/rewinds it by at most \p |n|
+/// positions.
+///
+/// \param[in] change_hashes A pointer to an `AMchangeHashes` struct.
+/// \param[in] n The direction (\p -n -> backward, \p +n -> forward) and maximum
+///              number of positions to advance/rewind.
+/// \return An `AMbyteSpan` struct that's `NULL` when \p change_hashes was
+///         previously advanced/rewound past its forward/backward limit.
+/// \pre \p change_hashes must be a valid address.
+/// \internal
+///
+/// #Safety
+/// change_hashes must be a pointer to a valid AMchangeHashes
+#[no_mangle]
+pub unsafe extern "C" fn AMchangeHashesNext(
+    change_hashes: *mut AMchangeHashes,
+    n: isize,
+) -> AMbyteSpan {
+    if let Some(change_hashes) = change_hashes.as_mut() {
+        if let Some(change_hash) = change_hashes.next(n) {
+            return change_hash.into();
+        }
+    }
+    AMbyteSpan::default()
+}
+
+/// \memberof AMchangeHashes
+/// \brief Advances/rewinds an `AMchangeHashes` struct by at most \p |n|
+/// positions and then gets the `AMbyteSpan` struct at its current position.
+///
+/// \param[in] change_hashes A pointer to an `AMchangeHashes` struct.
+/// \param[in] n The direction (\p -n -> backward, \p +n -> forward) and maximum
+///              number of positions to advance/rewind.
+/// \return An `AMbyteSpan` struct that's `NULL` when \p change_hashes is
+///         presently advanced/rewound past its forward/backward limit.
+/// \pre \p change_hashes must be a valid address.
+/// \internal
+///
+/// #Safety
+/// change_hashes must be a pointer to a valid AMchangeHashes
+#[no_mangle]
+pub unsafe extern "C" fn AMchangeHashesPrev(
+    change_hashes: *mut AMchangeHashes,
+    n: isize,
+) -> AMbyteSpan {
+    if let Some(change_hashes) = change_hashes.as_mut() {
+        if let Some(change_hash) = change_hashes.prev(n) {
+            return change_hash.into();
+        }
+    }
+    AMbyteSpan::default()
 }
 
 /// \memberof AMchangeHashes
@@ -101,59 +205,4 @@ pub unsafe extern "C" fn AMchangeHashesSize(change_hashes: *const AMchangeHashes
     } else {
         0
     }
-}
-
-/// \memberof AMchangeHashes
-/// \brief Gets the `AMbyteSpan` struct at the current position of an
-/// `AMchangeHashes`struct and then advances/rewinds it by at most \p |n|
-/// positions.
-///
-/// \param[in] change_hashes A pointer to an `AMchangeHashes` struct.
-/// \param[in] n The direction (\p -n -> backward, \p +n -> forward) and maximum
-/// number of positions to advance/rewind.
-/// \return An `AMbyteSpan` struct that's invalid when \p change_hashes was
-/// previously advanced/rewound past its forward/backward limit.
-/// \pre \p change_hashes must be a valid address.
-/// \internal
-///
-/// #Safety
-/// change_hashes must be a pointer to a valid AMchangeHashes
-#[no_mangle]
-pub unsafe extern "C" fn AMnextChangeHash(
-    change_hashes: *mut AMchangeHashes,
-    n: isize,
-) -> AMbyteSpan {
-    if let Some(change_hashes) = change_hashes.as_mut() {
-        if let Some(change_hash) = change_hashes.next(n) {
-            return change_hash.into();
-        }
-    }
-    AMbyteSpan::default()
-}
-
-/// \memberof AMchangeHashes
-/// \brief Advances/rewinds an `AMchangeHashes` struct by at most \p |n|
-/// positions and then gets the `AMbyteSpan` struct at its current position.
-///
-/// \param[in] change_hashes A pointer to an `AMchangeHashes` struct.
-/// \param[in] n The direction (\p -n -> backward, \p +n -> forward) and maximum
-/// number of positions to advance/rewind.
-/// \return An `AMbyteSpan` struct that's invalid when \p change_hashes is
-/// presently advanced/rewound past its forward/backward limit.
-/// \pre \p change_hashes must be a valid address.
-/// \internal
-///
-/// #Safety
-/// change_hashes must be a pointer to a valid AMchangeHashes
-#[no_mangle]
-pub unsafe extern "C" fn AMprevChangeHash(
-    change_hashes: *mut AMchangeHashes,
-    n: isize,
-) -> AMbyteSpan {
-    if let Some(change_hashes) = change_hashes.as_mut() {
-        if let Some(change_hash) = change_hashes.prev(n) {
-            return change_hash.into();
-        }
-    }
-    AMbyteSpan::default()
 }
