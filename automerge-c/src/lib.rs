38e4450d02e81639a93cc8fc27c4be1e696ac05c
Brian Gomberg <brian@audigolabs.com>
2022-09-27 16:00:07 -0700

    Fixed bug with length not being set

--- automerge-c/src/lib.rs
+++ automerge-c/src/lib.rs
@@ -40,6 +40,7 @@ impl Deref for Backend {
 
 unsafe fn from_buf_raw<T>(ptr: *const T, elts: usize) -> Vec<T> {
     let mut dst = Vec::with_capacity(elts);
+    dst.set_len(elts);
     ptr::copy(ptr, dst.as_mut_ptr(), elts);
     dst
 }
