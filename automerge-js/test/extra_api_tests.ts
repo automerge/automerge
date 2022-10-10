ed90699c708d39222495970100e2520ab9b55e57
Orion Henry <orion.henry@gmail.com>
2022-10-10 19:23:22 -0400

    add loadIncremental to js

---
+++ automerge-js/test/extra_api_tests.ts
@@ -0,0 +1,20 @@
+
+import * as assert from 'assert'
+import * as Automerge from '../src'
+
+describe('Automerge', () => {
+    describe('basics', () => {
+        it('should allow you to load incrementally', () => {
+          let doc1 = Automerge.from({ foo: "bar" })
+          let doc2 = Automerge.init();
+          doc2 = Automerge.loadIncremental(doc2, Automerge.save(doc1))
+          doc1 = Automerge.change(doc1, (d) => d.foo2 = "bar2")
+          doc2 = Automerge.loadIncremental(doc2, Automerge.getBackend(doc1).saveIncremental() )
+          doc1 = Automerge.change(doc1, (d) => d.foo = "bar2")
+          doc2 = Automerge.loadIncremental(doc2, Automerge.getBackend(doc1).saveIncremental() )
+          doc1 = Automerge.change(doc1, (d) => d.x = "y")
+          doc2 = Automerge.loadIncremental(doc2, Automerge.getBackend(doc1).saveIncremental() )
+          assert.deepEqual(doc1,doc2)
+        })
+    })
+})
