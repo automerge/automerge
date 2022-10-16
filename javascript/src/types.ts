24dcf8270a4ea64b064d9493fa7ee07ab984f027
Alex Good <alex@memoryandthought.me>
2022-10-16 23:28:40 +0100

    Add typedoc comments to the entire public JS API

--- javascript/src/types.ts
+++ javascript/src/types.ts
@@ -1,11 +1,12 @@
 
+import { Text } from "./text"
 export { Text } from "./text"
 export { Counter  } from "./counter"
 export { Int, Uint, Float64  } from "./numbers"
 
 import { Counter } from "./counter"
 
-export type AutomergeValue = ScalarValue | { [key: string]: AutomergeValue } | Array<AutomergeValue>
+export type AutomergeValue = ScalarValue | { [key: string]: AutomergeValue } | Array<AutomergeValue> | Text
 export type MapValue =  { [key: string]: AutomergeValue }
 export type ListValue = Array<AutomergeValue> 
 export type TextValue = Array<AutomergeValue>
