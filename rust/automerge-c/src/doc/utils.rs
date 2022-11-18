3dd954d5b77e6839b3752302a5800ed33afe1757
Jason Kankiewicz <jason.kankiewicz@gmail.com>
2022-11-18 09:31:04 -0800

    Moved the `to_obj_id` macro in with `AMobjId`.

--- rust/automerge-c/src/doc/utils.rs
+++ rust/automerge-c/src/doc/utils.rs
@@ -1,18 +1,3 @@
-use std::ffi::CStr;
-use std::os::raw::c_char;
-
-macro_rules! to_actor_id {
-    ($handle:expr) => {{
-        let handle = $handle.as_ref();
-        match handle {
-            Some(b) => b,
-            None => return AMresult::err("Invalid AMactorId pointer").into(),
-        }
-    }};
-}
-
-pub(crate) use to_actor_id;
-
 macro_rules! to_doc {
     ($handle:expr) => {{
         let handle = $handle.as_ref();
@@ -36,22 +21,3 @@ macro_rules! to_doc_mut {
 }
 
 pub(crate) use to_doc_mut;
-
-macro_rules! to_obj_id {
-    ($handle:expr) => {{
-        match $handle.as_ref() {
-            Some(obj_id) => obj_id,
-            None => &automerge::ROOT,
-        }
-    }};
-}
-
-pub(crate) use to_obj_id;
-
-pub(crate) unsafe fn to_str(c: *const c_char) -> String {
-    if !c.is_null() {
-        CStr::from_ptr(c).to_string_lossy().to_string()
-    } else {
-        String::default()
-    }
-}
