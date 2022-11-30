e0b2bc995ae426c8f1b5c2433c14252815cdc6f5
Andrew Jeffery <dev@jeffas.io>
2022-11-30 12:57:59 +0000

    Update nix flake and add formatter and dead code check (#466)
    * Add formatter for flake
    
    * Update flake inputs
    
    * Remove unused vars in flake
    
    * Add deadnix check and fixup devshells naming

--- flake.nix
+++ flake.nix
@@ -3,63 +3,66 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            overlays = [ rust-overlay.overlay ];
-            inherit system;
-          };
-          lib = pkgs.lib;
-          rust = pkgs.rust-bin.stable.latest.default;
-          cargoNix = pkgs.callPackage ./Cargo.nix {
-            inherit pkgs;
-            release = true;
-          };
-          debugCargoNix = pkgs.callPackage ./Cargo.nix {
-            inherit pkgs;
-            release = false;
-          };
-        in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs;
-              [
-                (rust.override {
-                  extensions = [ "rust-src" ];
-                  targets = [ "wasm32-unknown-unknown" ];
-                })
-                cargo-edit
-                cargo-watch
-                cargo-criterion
-                cargo-fuzz
-                cargo-flamegraph
-                cargo-deny
-                crate2nix
-                wasm-pack
-                pkgconfig
-                openssl
-                gnuplot
+    (system: let
+      pkgs = import nixpkgs {
+        overlays = [rust-overlay.overlays.default];
+        inherit system;
+      };
+      rust = pkgs.rust-bin.stable.latest.default;
+    in {
+      formatter = pkgs.alejandra;
 
-                nodejs
-                yarn
+      packages = {
+        deadnix = pkgs.runCommand "deadnix" {} ''
+          ${pkgs.deadnix}/bin/deadnix --fail ${./.}
+          mkdir $out
+        '';
+      };
 
-                # c deps
-                cmake
-                cmocka
-                doxygen
+      checks = {
+        inherit (self.packages.${system}) deadnix;
+      };
 
-                rnix-lsp
-                nixpkgs-fmt
-              ];
-          };
-        });
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (rust.override {
+            extensions = ["rust-src"];
+            targets = ["wasm32-unknown-unknown"];
+          })
+          cargo-edit
+          cargo-watch
+          cargo-criterion
+          cargo-fuzz
+          cargo-flamegraph
+          cargo-deny
+          crate2nix
+          wasm-pack
+          pkgconfig
+          openssl
+          gnuplot
+
+          nodejs
+          yarn
+
+          # c deps
+          cmake
+          cmocka
+          doxygen
+
+          rnix-lsp
+          nixpkgs-fmt
+        ];
+      };
+    });
 }
