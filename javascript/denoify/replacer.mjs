4ffe4d6238e65ab9f5bb49a31c291206d3d505b9
Alex Currie-Clark <acurrieclark@gmail.com>
2023-01-12 07:06:29 +0000

    Add denoify, a deno smoke test and add deno specific uuid implementation

---
+++ javascript/denoify/replacer.mjs
@@ -0,0 +1,43 @@
+// @denoify-ignore
+
+import { makeThisModuleAnExecutableReplacer } from "denoify"
+// import { assert } from "tsafe";
+// import * as path from "path";
+
+makeThisModuleAnExecutableReplacer(
+  async ({ parsedImportExportStatement, destDirPath, version }) => {
+    switch (parsedImportExportStatement.parsedArgument.nodeModuleName) {
+      case "@automerge/automerge-wasm":
+        {
+          /*
+           *We expect not to run against statements like
+           *import(..).then(...)
+           *or
+           *export * from "..."
+           *in our code.
+           */
+          if (
+            !parsedImportExportStatement.isAsyncImport &&
+            (parsedImportExportStatement.statementType === "import" ||
+              parsedImportExportStatement.statementType === "export")
+          ) {
+            // if (parsedImportExportStatement.isTypeOnly) {
+            //   return `${parsedImportExportStatement.statementType} type ${parsedImportExportStatement.target} from "https://deno.land/x/automerge_wasm@${version}/index.d.ts";`
+            // } else {
+            //   return `${parsedImportExportStatement.statementType} ${parsedImportExportStatement.target} from "https://deno.land/x/automerge_wasm@${version}/automerge_wasm.js";`
+            // }
+
+            if (parsedImportExportStatement.isTypeOnly) {
+              return `${parsedImportExportStatement.statementType} type ${parsedImportExportStatement.target} from "https://raw.githubusercontent.com/onsetsoftware/automerge-rs/js/automerge-wasm-0.1.20-alpha.1/deno_wasm_dist/index.d.ts";`
+            } else {
+              return `${parsedImportExportStatement.statementType} ${parsedImportExportStatement.target} from "https://raw.githubusercontent.com/onsetsoftware/automerge-rs/js/automerge-wasm-0.1.20-alpha.1/deno_wasm_dist/automerge_wasm.js";`
+            }
+          }
+        }
+        break
+    }
+
+    //The replacer should return undefined when we want to let denoify replace the statement
+    return undefined
+  }
+)
