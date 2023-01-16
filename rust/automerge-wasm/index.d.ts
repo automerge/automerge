d7bcd69cc773daa90c99105a35f23e2090af8e70
Alex Currie-Clark <acurrieclark@gmail.com>
2023-01-16 19:22:21 +0000

    Add Splice patch type to exports

--- rust/automerge-wasm/index.d.ts
+++ rust/automerge-wasm/index.d.ts
@@ -94,7 +94,7 @@ export type Op = {
   pred: string[],
 }
 
-export type Patch =  PutPatch | DelPatch | SplicePatch | IncPatch | InsertPatch;
+export type Patch =  PutPatch | DelPatch | SpliceTextPatch | IncPatch | InsertPatch;
 
 export type PutPatch = {
   action: 'put'
