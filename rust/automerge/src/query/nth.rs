8760996c7373c083f1a9b4a7bb8e07d1946f77bc
Orion Henry <orion.henry@gmail.com>
2023-04-04 09:16:53 -0700

    wip

--- rust/automerge/src/query/nth.rs
+++ rust/automerge/src/query/nth.rs
@@ -11,24 +11,16 @@ use std::fmt::Debug;
 pub(crate) struct Nth<'a> {
     target: usize,
     seen: usize,
-    encoding: ListEncoding,
-    last_width: usize,
-    /// last_seen is the target elemid of the last `seen` operation.
-    /// It is used to avoid double counting visible elements (which arise through conflicts) that are split across nodes.
-    last_seen: Option<Key>,
     pub(crate) ops: Vec<&'a Op>,
     pub(crate) ops_pos: Vec<usize>,
     pub(crate) pos: usize,
 }
 
 impl<'a> Nth<'a> {
-    pub(crate) fn new(target: usize, encoding: ListEncoding) -> Self {
+    pub(crate) fn new(target: usize, _encoding: ListEncoding) -> Self {
         Nth {
             target,
             seen: 0,
-            last_width: 1,
-            encoding,
-            last_seen: None,
             ops: vec![],
             ops_pos: vec![],
             pos: 0,
@@ -50,7 +42,7 @@ impl<'a> Nth<'a> {
     }
 
     pub(crate) fn index(&self) -> usize {
-        self.seen - self.last_width
+        self.seen - 1
     }
 }
 
@@ -63,8 +55,7 @@ impl<'a> TreeQuery<'a> for Nth<'a> {
         if let Some((index, pos)) = &tree.last_insert {
             if *index == self.target {
                 if let Some(op) = tree.internal.get(*pos) {
-                    self.last_width = op.width(self.encoding);
-                    self.seen = *index + self.last_width;
+                    self.seen = *index + 1;
                     self.ops.push(op);
                     self.ops_pos.push(*pos);
                     self.pos = *pos + 1;
@@ -75,68 +66,29 @@ impl<'a> TreeQuery<'a> for Nth<'a> {
         false
     }
 
-    fn query_node(&mut self, child: &OpTreeNode, ops: &[Op]) -> QueryResult {
-        // We note the number of values stored in / below the node `child`
-        let mut num_vis = child.index.visible_len(self.encoding);
-        // Nodes are sorted by key (obj, prop, ?) and time. We can only see a key twice as
-        // visible if it is the last element and has a conflict and occurs as visible again in
-        // the next node. To prevent double-counting it, we subtract 1 (to pretend we didn't see
-        // it yet 🫣).
-        if let Some(last_seen) = self.last_seen {
-            if child.index.has_visible(&last_seen) {
-                num_vis -= 1;
-            }
+    fn query_node(&mut self, node: &OpTreeNode, _ops: &[Op]) -> QueryResult {
+        let num_vis = node.index.visible.len();
+        if self.seen + num_vis >= self.target {
+            return QueryResult::Descend;
         }
-
-        if self.seen + num_vis > self.target {
-            // Enter this node as the nth element is in this node
-            QueryResult::Descend
-        } else {
-            // skip this node as no useful ops in it
-            self.pos += child.len();
-            self.seen += num_vis;
-
-            // We have updated seen by the number of visible elements in this index, before we skip it.
-            // We also need to keep track of the last elemid that we have seen (and counted as seen).
-            // We can just use the elemid of the last op in this node as either:
-            // - the insert was at a previous node and this is a long run of overwrites so last_seen should already be set correctly
-            // - the visible op is in this node and the elemid references it so it can be set here
-            // - the visible op is in a future node and so it will be counted as seen there
-            // ⚠️ We also need to reset last_seen if it is set to something else than the last item
-            //   in the child. This means that the child contains an `insert` (so last_seen should
-            //   be reset to None), but no visible op (so last_seen should not be set to a new value)
-            //   The visible op also cannot be in a previous node, because then `last_seen` would
-            //   already be set to the same elemid as the last element in the child.
-            let last_elemid = ops[child.last()].elemid_or_key();
-            if child.index.has_visible(&last_elemid) {
-                self.last_seen = Some(last_elemid);
-            } else if self.last_seen.is_some() && Some(last_elemid) != self.last_seen {
-                self.last_seen = None;
-            }
-            QueryResult::Next
-        }
+        self.seen += num_vis;
+        self.pos += node.len();
+        QueryResult::Next
     }
 
     fn query_element(&mut self, element: &'a Op) -> QueryResult {
-        if element.insert {
-            if self.seen > self.target {
-                return QueryResult::Finish;
+        if element.insert && self.seen > self.target {
+            QueryResult::Finish
+        } else {
+            if element.visible() {
+                self.seen += 1;
+                if self.seen > self.target {
+                    self.ops.push(element);
+                    self.ops_pos.push(self.pos);
+                }
             }
-            // we have a new potentially visible element so reset last_seen
-            self.last_seen = None
+            self.pos += 1;
+            QueryResult::Next
         }
-        let visible = element.visible();
-        if visible && self.last_seen.is_none() {
-            self.last_width = element.width(self.encoding);
-            self.seen += self.last_width;
-            // we have a new visible element
-            self.last_seen = Some(element.elemid_or_key());
-        }
-        if self.seen > self.target && visible {
-            self.ops.push(element);
-            self.ops_pos.push(self.pos);
-        }
-        self.pos += 1;
-        QueryResult::Next
     }
 }
