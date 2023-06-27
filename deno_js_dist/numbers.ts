40b245df6e23cb1ef509edb770bb5ff024380faa
actions <actions@github.com>
2023-06-27 00:07:31 +0000

    Add deno js release files

---
+++ deno_js_dist/numbers.ts
@@ -0,0 +1,54 @@
+// Convenience classes to allow users to strictly specify the number type they want
+
+import { INT, UINT, F64 } from "./constants.ts"
+
+export class Int {
+  value: number
+
+  constructor(value: number) {
+    if (
+      !(
+        Number.isInteger(value) &&
+        value <= Number.MAX_SAFE_INTEGER &&
+        value >= Number.MIN_SAFE_INTEGER
+      )
+    ) {
+      throw new RangeError(`Value ${value} cannot be a uint`)
+    }
+    this.value = value
+    Reflect.defineProperty(this, INT, { value: true })
+    Object.freeze(this)
+  }
+}
+
+export class Uint {
+  value: number
+
+  constructor(value: number) {
+    if (
+      !(
+        Number.isInteger(value) &&
+        value <= Number.MAX_SAFE_INTEGER &&
+        value >= 0
+      )
+    ) {
+      throw new RangeError(`Value ${value} cannot be a uint`)
+    }
+    this.value = value
+    Reflect.defineProperty(this, UINT, { value: true })
+    Object.freeze(this)
+  }
+}
+
+export class Float64 {
+  value: number
+
+  constructor(value: number) {
+    if (typeof value !== "number") {
+      throw new RangeError(`Value ${value} cannot be a float64`)
+    }
+    this.value = value || 0.0
+    Reflect.defineProperty(this, F64, { value: true })
+    Object.freeze(this)
+  }
+}
---
+++ deno_js_dist/uuid.ts
@@ -0,0 +1,26 @@
+import * as v4 from "https://deno.land/x/uuid@v0.1.2/mod.ts"
+
+// this file is a deno only port of the uuid module
+
+function defaultFactory() {
+  return v4.uuid().replace(/-/g, "")
+}
+
+let factory = defaultFactory
+
+interface UUIDFactory extends Function {
+  setFactory(f: typeof factory): void
+  reset(): void
+}
+
+export const uuid: UUIDFactory = () => {
+  return factory()
+}
+
+uuid.setFactory = newFactory => {
+  factory = newFactory
+}
+
+uuid.reset = () => {
+  factory = defaultFactory
+}
