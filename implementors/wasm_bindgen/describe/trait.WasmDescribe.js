b340bbe90356e20857c4f22d159a4f89ba552e2f
orionz <orionz@users.noreply.github.com>
2023-08-02 05:59:13 +0000

    deploy: 2cbd48615a8b6ca3d4f726b9576110c89e17da19

--- implementors/wasm_bindgen/describe/trait.WasmDescribe.js
+++ implementors/wasm_bindgen/describe/trait.WasmDescribe.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"automerge_wasm":[["impl WasmDescribe for <a class=\"enum\" href=\"automerge_wasm/enum.TextRepresentation.html\" title=\"enum automerge_wasm::TextRepresentation\">TextRepresentation</a>"],["impl WasmDescribe for <a class=\"struct\" href=\"automerge_wasm/struct.Automerge.html\" title=\"struct automerge_wasm::Automerge\">Automerge</a>"]]
+"automerge_wasm":[["impl WasmDescribe for <a class=\"struct\" href=\"automerge_wasm/struct.Automerge.html\" title=\"struct automerge_wasm::Automerge\">Automerge</a>"],["impl WasmDescribe for <a class=\"enum\" href=\"automerge_wasm/enum.TextRepresentation.html\" title=\"enum automerge_wasm::TextRepresentation\">TextRepresentation</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
