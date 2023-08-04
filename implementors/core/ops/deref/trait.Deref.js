9cc42dd84d83793d2f00852d20e8e3fecc1715b1
alexjg <alexjg@users.noreply.github.com>
2023-08-04 09:41:57 +0000

    deploy: 1af1003f36a6660de19303a4f132fe501609f7aa

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"automerge":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"automerge/hydrate/struct.Map.html\" title=\"struct automerge::hydrate::Map\">Map</a>"]]
+"automerge":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"automerge/hydrate/struct.Map.html\" title=\"struct automerge::hydrate::Map\">Map</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
