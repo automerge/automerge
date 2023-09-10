9f7efc8de69e4e50eba3b95021fda758759b4fb5
Partha Protim Paul <ppartha101@gmail.com>
2023-09-10 19:33:28 +0600

    added toString() method to RawString object (#729)
    Fixes #722

--- javascript/src/raw_string.ts
+++ javascript/src/raw_string.ts
@@ -3,4 +3,11 @@ export class RawString {
   constructor(val: string) {
     this.val = val
   }
+
+  /**
+   * Returns the content of the RawString object as a simple string
+   */
+  toString(): string {
+    return this.val;
+  }
 }
