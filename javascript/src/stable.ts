d0aa92dbb888566e5d85e415fb6e631c8f41e7f3
Alex Good <alex@memoryandthought.me>
2023-09-19 14:28:37 +0100

    Fix prettier

--- javascript/src/stable.ts
+++ javascript/src/stable.ts
@@ -59,9 +59,9 @@ const SyncStateSymbol = Symbol("_syncstate")
 /**
  * An opaque type tracking the state of sync with a remote peer
  */
-type SyncState = JsSyncState & { 
+type SyncState = JsSyncState & {
   /** @hidden */
-  _opaque: typeof SyncStateSymbol 
+  _opaque: typeof SyncStateSymbol
 }
 
 import { ApiHandler, type ChangeToEncode, UseApi } from "./low_level.js"
