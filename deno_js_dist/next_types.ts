1aa3180efe69e553dc6f553342b43515d55bd969
actions <actions@github.com>
2023-09-22 17:06:20 +0000

    Add deno js release files

---
+++ deno_js_dist/next_types.ts
@@ -0,0 +1,36 @@
+import { Counter } from "./types.ts"
+
+export {
+  Counter,
+  type Doc,
+  Int,
+  Uint,
+  Float64,
+  type Patch,
+  type PatchCallback,
+  type Mark,
+  type MarkRange,
+  type MarkValue,
+  type Cursor,
+  type PatchInfo,
+  type PatchSource,
+} from "./types.ts"
+
+import { RawString } from "./raw_string.ts"
+export { RawString } from "./raw_string.ts"
+
+export type AutomergeValue =
+  | ScalarValue
+  | { [key: string]: AutomergeValue }
+  | Array<AutomergeValue>
+export type MapValue = { [key: string]: AutomergeValue }
+export type ListValue = Array<AutomergeValue>
+export type ScalarValue =
+  | string
+  | number
+  | null
+  | boolean
+  | Date
+  | Counter
+  | Uint8Array
+  | RawString
