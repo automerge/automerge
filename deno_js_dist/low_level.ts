0f60ddd11c5be27ef8d48592aac0edca9844dff4
actions <actions@github.com>
2023-10-28 18:24:38 +0000

    Add deno js release files

---
+++ deno_js_dist/low_level.ts
@@ -0,0 +1,59 @@
+import {
+  type API,
+  Automerge,
+  type Change,
+  type DecodedChange,
+  SyncState,
+  type SyncMessage,
+  type JsSyncState,
+  type DecodedSyncMessage,
+  type ChangeToEncode,
+  type LoadOptions,
+  type InitOptions,
+} from "https://deno.land/x/automerge_wasm@0.4.0/automerge_wasm.js";
+export type { ChangeToEncode } from "https://deno.land/x/automerge_wasm@0.4.0/index.d.ts";
+
+export function UseApi(api: API) {
+  for (const k in api) {
+    // eslint-disable-next-line @typescript-eslint/no-extra-semi,@typescript-eslint/no-explicit-any
+    ;(ApiHandler as any)[k] = (api as any)[k]
+  }
+}
+
+/* eslint-disable */
+export const ApiHandler: API = {
+  create(options?: InitOptions): Automerge {
+    throw new RangeError("Automerge.use() not called")
+  },
+  load(data: Uint8Array, options?: LoadOptions): Automerge {
+    throw new RangeError("Automerge.use() not called (load)")
+  },
+  encodeChange(change: ChangeToEncode): Change {
+    throw new RangeError("Automerge.use() not called (encodeChange)")
+  },
+  decodeChange(change: Change): DecodedChange {
+    throw new RangeError("Automerge.use() not called (decodeChange)")
+  },
+  initSyncState(): SyncState {
+    throw new RangeError("Automerge.use() not called (initSyncState)")
+  },
+  encodeSyncMessage(message: DecodedSyncMessage): SyncMessage {
+    throw new RangeError("Automerge.use() not called (encodeSyncMessage)")
+  },
+  decodeSyncMessage(msg: SyncMessage): DecodedSyncMessage {
+    throw new RangeError("Automerge.use() not called (decodeSyncMessage)")
+  },
+  encodeSyncState(state: SyncState): Uint8Array {
+    throw new RangeError("Automerge.use() not called (encodeSyncState)")
+  },
+  decodeSyncState(data: Uint8Array): SyncState {
+    throw new RangeError("Automerge.use() not called (decodeSyncState)")
+  },
+  exportSyncState(state: SyncState): JsSyncState {
+    throw new RangeError("Automerge.use() not called (exportSyncState)")
+  },
+  importSyncState(state: JsSyncState): SyncState {
+    throw new RangeError("Automerge.use() not called (importSyncState)")
+  },
+}
+/* eslint-enable */
