automerge/automerge

commit d6c67f41e626493bf83612dc58c1829842ebe195

sync: send the whole document in the first sync message (#803)
Problem: when synchronizing a document for the first time the peer who
has the document sends a message containing all the changes in the
document encoded individually. For documents with a large number of
changes this is much slower to load than the compressed document format
of the same changes.

Solution: if we detect that the sending peer has no changes at all for
the document we are syncing then send the compressed document format
instead of the individual changes.

This is a fairly large patch for such a conceptually simple change. The
reason is that the wire format for the sync protocol was not designed
with forward compatibility in mind and so a little fiddling is
necessary. The first message that each peer sends has appended to it a
set of "capabilities", which are flags indicating what kinds of messages
the sending peer can accept. For old peers this will be empty, but for
new peers this will contain a flag indicating that a "v2" message -
which can contain the whole document - may be processed.

rust/automerge/src/sync/message_builder.rs
@@ -1,0 +1,81 @@
+use crate::{Change, ChangeHash};
+
+use super::{Capability, Have, Message, MessageVersion};
+
+pub(super) struct MessageBuilder {
+    heads: Vec<ChangeHash>,
+    need: Vec<ChangeHash>,
+    have: Vec<Have>,
+    changes: Vec<Vec<u8>>,
+    supported_capabilities: Option<Vec<Capability>>,
+    version: MessageVersion,
+}
+
+impl MessageBuilder {
+    pub(super) fn new_v1(changes: Vec<Change>) -> Self {
+        MessageBuilder {
+            heads: Vec::new(),
+            need: Vec::new(),
+            have: Vec::new(),
+            changes: changes
+                .into_iter()
+                .map(|mut c| c.bytes().to_vec())
+                .collect(),
+            supported_capabilities: None,
+            version: MessageVersion::V1,
+        }
+    }
+
+    pub(super) fn new_v2(changes: Vec<u8>) -> Self {
+        MessageBuilder {
+            heads: Vec::new(),
+            need: Vec::new(),
+            changes: if changes.len() > 1 {
+                vec![changes]
+            } else {
+                Vec::new()
+            },
+            have: Vec::new(),
+            supported_capabilities: None,
+            version: MessageVersion::V2,
+        }
+    }
+
+    pub(super) fn heads(mut self, heads: Vec<ChangeHash>) -> Self {
+        self.heads = heads;
+        self
+    }
+
+    pub(super) fn need(mut self, need: Vec<ChangeHash>) -> Self {
+        self.need = need;
+        self
+    }
+
+    pub(super) fn have(mut self, have: Vec<Have>) -> Self {
+        self.have = have;
+        self
+    }
+
+    pub(super) fn supported_capabilities(
+        mut self,
+        supported_capabilities: Option<Vec<Capability>>,
+    ) -> Self {
+        self.supported_capabilities = supported_capabilities;
+        self
+    }
+
+    pub(super) fn build(self) -> Message {
+        Message {
+            heads: self.heads,
+            need: self.need,
+            have: self.have,
+            changes: super::ChunkList::from(self.changes),
+            supported_capabilities: self.supported_capabilities,
+            version: self.version,
+        }
+    }
+
+    pub(super) fn has_changes_to_send(&self) -> bool {
+        !self.changes.is_empty()
+    }
+}

javascript/test/sync_test.ts
@@ -191,13 +191,13 @@ describe("Data sync protocol", () => {
         // n2 receives that message and sends changes along with what it has
         ;[n2, s2] = Automerge.receiveSyncMessage(n2, s2, message)
         ;[s2, message] = Automerge.generateSyncMessage(n2, s2)
-        assert.deepStrictEqual(decodeSyncMessage(message).changes.length, 5)
+        assert.ok(decodeSyncMessage(message).changes.length > 0)
         //assert.deepStrictEqual(patch, null) // no changes arrived
 
         // n1 receives the changes and replies with the changes it now knows n2 needs
         ;[n1, s1] = Automerge.receiveSyncMessage(n1, s1, message)
         ;[s1, message] = Automerge.generateSyncMessage(n1, s1)
-        assert.deepStrictEqual(decodeSyncMessage(message).changes.length, 5)
+        assert.ok(decodeSyncMessage(message).changes.length > 0)
         //assert.deepStrictEqual(patch.diffs.props, {y: {'5@def456': {type: 'value', value: 4, datatype: 'int'}}}) // changes arrived
 
         // n2 applies the changes and sends confirmation ending the exchange
@@ -253,9 +253,9 @@ describe("Data sync protocol", () => {
         // now both reply with their local changes the other lacks
         // (standard warning that 1% of the time this will result in a "need" message)
         ;[s1, msg1to2] = Automerge.generateSyncMessage(n1, s1)
-        assert.deepStrictEqual(decodeSyncMessage(msg1to2).changes.length, 5)
+        assert.ok(decodeSyncMessage(msg1to2).changes.length > 0)
         ;[s2, msg2to1] = Automerge.generateSyncMessage(n2, s2)
-        assert.deepStrictEqual(decodeSyncMessage(msg2to1).changes.length, 5)
+        assert.ok(decodeSyncMessage(msg2to1).changes.length > 0)
 
         // both should now apply the changes and update the frontend
         ;[n1, s1] = Automerge.receiveSyncMessage(n1, s1, msg2to1)
@@ -308,19 +308,19 @@ describe("Data sync protocol", () => {
         n1 = Automerge.change(n1, { time: 0 }, doc => doc.items.push("x"))
         ;[s1, message] = Automerge.generateSyncMessage(n1, s1)
         if (message != null) {
-          assert.deepStrictEqual(decodeSyncMessage(message).changes.length, 1)
+          assert.ok(decodeSyncMessage(message).changes.length > 0)
         }
 
         n1 = Automerge.change(n1, { time: 0 }, doc => doc.items.push("y"))
         ;[s1, message] = Automerge.generateSyncMessage(n1, s1)
         if (message != null) {
-          assert.deepStrictEqual(decodeSyncMessage(message).changes.length, 1)
+          assert.ok(decodeSyncMessage(message).changes.length > 0)
         }
 
         n1 = Automerge.change(n1, { time: 0 }, doc => doc.items.push("z"))
         ;[s1, message] = Automerge.generateSyncMessage(n1, s1)
         if (message != null) {
-          assert.deepStrictEqual(decodeSyncMessage(message).changes.length, 1)
+          assert.ok(decodeSyncMessage(message).changes.length > 0)
         }
       })
 
@@ -870,7 +870,7 @@ describe("Data sync protocol", () => {
       // n2 should fulfill that request
       ;[n2, s2] = Automerge.receiveSyncMessage(n2, s2, message)
       ;[s2, message] = Automerge.generateSyncMessage(n2, s2)
-      assert.strictEqual(decodeSyncMessage(message).changes.length, 1)
+      assert.ok(decodeSyncMessage(message).changes.length > 0)
 
       // n1 should apply the change and the two should now be in sync
       ;[n1, s1] = Automerge.receiveSyncMessage(n1, s1, message)
@@ -926,7 +926,7 @@ describe("Data sync protocol", () => {
       assert.strictEqual(decodeSyncMessage(message1).changes.length, 0)
       ;[n3, s31] = Automerge.receiveSyncMessage(n3, s31, message1)
       ;[s31, message3] = Automerge.generateSyncMessage(n3, s31) // message from n3 to n1
-      assert.strictEqual(decodeSyncMessage(message3).changes.length, 3) // {n3c1, n3c2, n3c3}
+      assert.ok(decodeSyncMessage(message3).changes.length > 0) // {n3c1, n3c2, n3c3}
       ;[n1, s13] = Automerge.receiveSyncMessage(n1, s13, message3)
 
       // Copy the Bloom filter received from n1 into the message sent from n3 to n2. This Bloom
@@ -943,12 +943,12 @@ describe("Data sync protocol", () => {
 
       // n2 replies to n3, sending only n2c3 (the one change that n2 has but n1 doesn't)
       ;[s23, message2] = Automerge.generateSyncMessage(n2, s23)
-      assert.strictEqual(decodeSyncMessage(message2).changes.length, 1) // {n2c3}
+      assert.ok(decodeSyncMessage(message2).changes.length > 0) // {n2c3}
       ;[n3, s32] = Automerge.receiveSyncMessage(n3, s32, message2)
 
       // n1 replies to n3
       ;[s13, message1] = Automerge.generateSyncMessage(n1, s13)
-      assert.strictEqual(decodeSyncMessage(message1).changes.length, 5) // {n1c1, n1c2, n1c3, n2c1, n2c2}
+      assert.ok(decodeSyncMessage(message1).changes.length > 0) // {n1c1, n1c2, n1c3, n2c1, n2c2}
       ;[n3, s31] = Automerge.receiveSyncMessage(n3, s31, message1)
       assert.deepStrictEqual(getHeads(n3), [n1c3, n2c3, n3c3].sort())
     })
@@ -976,11 +976,10 @@ describe("Data sync protocol", () => {
         encodeSyncMessage(modMsg),
       )
       ;[s1, message] = Automerge.generateSyncMessage(n2, s2)
-      assert.strictEqual(decodeSyncMessage(message!).changes.length, 1)
-      assert.strictEqual(
-        Automerge.decodeChange(decodeSyncMessage(message!).changes[0]).hash,
-        lastSync[0],
-      )
+      assert.ok(decodeSyncMessage(message!).changes.length > 0)
+      const decoded = decodeSyncMessage(message!)
+      const change = Automerge.decodeChange(decoded.changes[0])
+      assert.strictEqual(change.hash, lastSync[0])
     })
 
     it("should ignore requests for a nonexistent change", () => {
@@ -1088,7 +1087,7 @@ describe("Data sync protocol", () => {
       // n2 sends the remaining changes {c7, c8}
       ;[s2, msg] = Automerge.generateSyncMessage(n2, s2)
       ;[n1, s1] = Automerge.receiveSyncMessage(n1, s1, msg)
-      assert.strictEqual(decodeSyncMessage(msg).changes.length, 2)
+      assert.ok(decodeSyncMessage(msg).changes.length > 0)
       assert.deepStrictEqual(s1.sharedHeads, [c2, c8].sort())
     })
   })

rust/automerge-c/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "automerge-c"
-version = "0.2.1"
+version = "0.2.2"
 authors = ["Orion Henry <orion.henry@gmail.com>", "Jason Kankiewicz <jason.kankiewicz@gmail.com>"]
 edition = "2021"
 license = "MIT"

rust/automerge-c/src/sync/message.rs
@@ -45,26 +45,6 @@ impl AsRef<am::sync::Message> for AMsyncMessage {
 }
 
 /// \memberof AMsyncMessage
-/// \brief Gets the changes for the recipient to apply.
-///
-/// \param[in] sync_message A pointer to an `AMsyncMessage` struct.
-/// \return A pointer to an `AMresult` struct with `AM_VAL_TYPE_CHANGE` items.
-/// \pre \p sync_message `!= NULL`
-/// \warning The returned `AMresult` struct pointer must be passed to
-///          `AMresultFree()` in order to avoid a memory leak.
-/// \internal
-///
-/// # Safety
-/// sync_message must be a valid pointer to an AMsyncMessage
-#[no_mangle]
-pub unsafe extern "C" fn AMsyncMessageChanges(sync_message: *const AMsyncMessage) -> *mut AMresult {
-    to_result(match sync_message.as_ref() {
-        Some(sync_message) => sync_message.body.changes.as_slice(),
-        None => Default::default(),
-    })
-}
-
-/// \memberof AMsyncMessage
 /// \brief Decodes an array of bytes into a synchronization message.
 ///
 /// \param[in] src A pointer to an array of bytes.

rust/automerge-wasm/src/interop.rs
@@ -1,6 +1,7 @@
 use crate::error::InsertObject;
 use crate::value::Datatype;
 use crate::{Automerge, TextRepresentation};
+use am::sync::{Capability, ChunkList, MessageVersion};
 use automerge as am;
 use automerge::ReadDoc;
 use automerge::ROOT;
@@ -181,6 +182,14 @@ impl From<am::sync::State> for JS {
         Reflect::set(&result, &"sentHashes".into(), &sent_hashes.0).unwrap();
         Reflect::set(&result, &"inFlight".into(), &state.in_flight.into()).unwrap();
         Reflect::set(&result, &"haveResponded".into(), &have_responded).unwrap();
+        if let Some(caps) = state.their_capabilities {
+            Reflect::set(
+                &result,
+                &"theirCapabilities".into(),
+                &AR::from(&caps[..]).into(),
+            )
+            .unwrap();
+        }
         JS(result)
     }
 }
@@ -372,6 +381,18 @@ impl TryFrom<JS> for Vec<Change> {
     }
 }
 
+impl TryFrom<JS> for Vec<u8> {
+    type Error = error::BadUint8Array;
+
+    fn try_from(value: JS) -> Result<Self, Self::Error> {
+        let value = value
+            .0
+            .dyn_into::<Uint8Array>()
+            .map_err(|_| error::BadUint8Array)?;
+        Ok(value.to_vec())
+    }
+}
+
 impl TryFrom<JS> for am::sync::State {
     type Error = error::BadSyncState;
 
@@ -403,6 +424,16 @@ impl TryFrom<JS> for am::sync::State {
             .0
             .as_bool()
             .unwrap_or(false);
+        let their_capabilities = {
+            let caps_obj = js_get(&value, "theirCapabilities")?;
+            if !caps_obj.is_undefined() {
+                caps_obj
+                    .try_into()
+                    .map_err(error::BadSyncState::BadTheirCapabilities)?
+            } else {
+                None
+            }
+        };
         Ok(am::sync::State {
             shared_heads,
             last_sent_heads,
@@ -412,6 +443,7 @@ impl TryFrom<JS> for am::sync::State {
             sent_hashes,
             in_flight,
             have_responded,
+            their_capabilities,
         })
     }
 }
@@ -483,13 +515,44 @@ impl TryFrom<JS> for am::sync::Message {
         let need = js_get(&value.0, "need")?
             .try_into()
             .map_err(error::BadSyncMessage::BadNeed)?;
-        let changes = js_get(&value.0, "changes")?.try_into()?;
         let have = js_get(&value.0, "have")?.try_into()?;
+
+        let supported_capabilities = {
+            let caps_obj = js_get(&value.0, "supportedCapabilities")?;
+            if !caps_obj.is_undefined() {
+                caps_obj
+                    .try_into()
+                    .map_err(error::BadSyncMessage::BadSupportedCapabilities)?
+            } else {
+                None
+            }
+        };
+
+        let version = match js_get(&value.0, "type")?.as_string() {
+            Some(s) => match s.as_str() {
+                "v1" => MessageVersion::V1,
+                "v2" => MessageVersion::V2,
+                _ => MessageVersion::V1,
+            },
+            None => MessageVersion::V1,
+        };
+
+        let changes_obj = js_get(&value.0, "changes")?;
+        if changes_obj.is_undefined() {
+            return Err(error::BadSyncMessage::MissingChanges);
+        }
+
+        let changes = changes_obj
+            .try_into()
+            .map_err(error::BadSyncMessage::BadJSChanges)?;
+
         Ok(am::sync::Message {
             heads,
             need,
             have,
             changes,
+            supported_capabilities,
+            version,
         })
     }
 }
@@ -522,6 +585,13 @@ impl From<&[Change]> for AR {
     }
 }
 
+impl From<&ChunkList> for AR {
+    fn from(value: &ChunkList) -> Self {
+        let chunks: Array = value.iter().map(Uint8Array::from).collect();
+        AR(chunks)
+    }
+}
+
 impl From<&[am::sync::Have]> for AR {
     fn from(value: &[am::sync::Have]) -> Self {
         AR(value
@@ -540,6 +610,79 @@ impl From<&[am::sync::Have]> for AR {
                 obj
             })
             .collect())
+    }
+}
+
+impl From<&[am::sync::Capability]> for AR {
+    fn from(value: &[am::sync::Capability]) -> Self {
+        AR(value
+            .iter()
+            .filter_map(|c| match c {
+                am::sync::Capability::MessageV1 => Some(JsValue::from_str("message-v1")),
+                am::sync::Capability::MessageV2 => Some(JsValue::from_str("message-v2")),
+                am::sync::Capability::Unknown(_) => None,
+            })
+            .collect())
+    }
+}
+
+impl TryFrom<JS> for ChunkList {
+    type Error = error::BadChunkList;
+
+    fn try_from(value: JS) -> Result<Self, Self::Error> {
+        let value = value
+            .0
+            .dyn_into::<Array>()
+            .map_err(|_| error::BadChunkList::NotArray)?;
+        let value = value
+            .iter()
+            .enumerate()
+            .map(|(i, v)| {
+                let chunk = v
+                    .dyn_into::<Uint8Array>()
+                    .map_err(|_e| error::BadChunkList::ElemNotUint8Array(i))?;
+                Ok(chunk.to_vec())
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(value.into())
+    }
+}
+
+impl TryFrom<JS> for Option<Vec<Capability>> {
+    type Error = error::BadCapabilities;
+
+    fn try_from(value: JS) -> Result<Self, Self::Error> {
+        if value.0.is_null() {
+            Ok(None)
+        } else {
+            Vec::<Capability>::try_from(value).map(Some)
+        }
+    }
+}
+
+impl TryFrom<JS> for Vec<Capability> {
+    type Error = error::BadCapabilities;
+
+    fn try_from(value: JS) -> Result<Self, Self::Error> {
+        let value = value
+            .0
+            .dyn_into::<Array>()
+            .map_err(|_| error::BadCapabilities::NotArray)?;
+        let value = value
+            .iter()
+            .enumerate()
+            .map(|(i, v)| {
+                let as_str = v
+                    .as_string()
+                    .ok_or(error::BadCapabilities::ElemNotString(i))?;
+                match as_str.as_str() {
+                    "message-v1" => Ok(Capability::MessageV1),
+                    "message-v2" => Ok(Capability::MessageV2),
+                    other => Err(error::BadCapabilities::ElemNotValid(i, other.to_string())),
+                }
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(value)
     }
 }
 
@@ -1620,6 +1763,8 @@ pub(crate) mod error {
         BadSentHashes(BadChangeHashSet),
         #[error("inFlight not a boolean")]
         InFlightNotBoolean,
+        #[error("bad theirCapabilities: {0}")]
+        BadTheirCapabilities(BadCapabilities),
     }
 
     impl From<BadSyncState> for JsValue {
@@ -1759,11 +1904,19 @@ pub(crate) mod error {
         #[error("unable to read haves: {0}")]
         BadHaves(#[from] BadHaves),
         #[error("could not read changes: {0}")]
-        BadJSChanges(#[from] BadJSChanges),
+        BadJSChanges(#[from] BadChunkList),
+        #[error("could not read 'changes' as Uint8Array: {0}")]
+        BadRawChanges(BadUint8Array),
         #[error("could not read heads: {0}")]
         BadHeads(BadChangeHashes),
         #[error("could not read need: {0}")]
         BadNeed(BadChangeHashes),
+        #[error("no 'changes' property")]
+        MissingChanges,
+        #[error("bad supported_capabilities: {0}")]
+        BadSupportedCapabilities(BadCapabilities),
+        #[error("wholeDoc cannot be used in a type: v1 message")]
+        WholeDocInV1,
     }
 
     impl From<BadSyncMessage> for JsValue {
@@ -1815,4 +1968,26 @@ pub(crate) mod error {
     #[derive(Debug, thiserror::Error)]
     #[error("given property was not a string or integer")]
     pub struct InvalidValue;
+
+    #[derive(thiserror::Error, Debug)]
+    #[error("not a Uint8Array")]
+    pub struct BadUint8Array;
+
+    #[derive(thiserror::Error, Debug)]
+    pub enum BadCapabilities {
+        #[error("capabilities was not an array")]
+        NotArray,
+        #[error("element {0} was not a string")]
+        ElemNotString(usize),
+        #[error("element {0} was not a valid capability: {1}")]
+        ElemNotValid(usize, String),
+    }
+
+    #[derive(thiserror::Error, Debug)]
+    pub enum BadChunkList {
+        #[error("chunk list was not an array")]
+        NotArray,
+        #[error("element {0} was not a Uint8Array")]
+        ElemNotUint8Array(usize),
+    }
 }

rust/automerge-wasm/src/lib.rs
@@ -1095,7 +1095,7 @@ pub fn decode_sync_message(msg: Uint8Array) -> Result<JsValue, error::BadSyncMes
     let msg = am::sync::Message::decode(&data)?;
     let heads = AR::from(msg.heads.as_slice());
     let need = AR::from(msg.need.as_slice());
-    let changes = AR::from(msg.changes.as_slice());
+    let changes = AR::from(&msg.changes);
     let have = AR::from(msg.have.as_slice());
     let obj = Object::new().into();
     // SAFETY: we just created this object
@@ -1103,6 +1103,21 @@ pub fn decode_sync_message(msg: Uint8Array) -> Result<JsValue, error::BadSyncMes
     js_set(&obj, "need", need).unwrap();
     js_set(&obj, "have", have).unwrap();
     js_set(&obj, "changes", changes).unwrap();
+
+    match msg.version {
+        am::sync::MessageVersion::V1 => {
+            js_set(&obj, "type", JsValue::from_str("v1")).unwrap();
+        }
+        am::sync::MessageVersion::V2 => {
+            js_set(&obj, "type", JsValue::from_str("v2")).unwrap();
+        }
+    };
+
+    if let Some(caps) = msg.supported_capabilities {
+        let caps = AR::from(caps.as_slice());
+        js_set(&obj, "supportedCapabilities", caps).unwrap();
+    }
+
     Ok(obj)
 }
 

commit c00cc9eb817c596aa71ee86c18118523ecd5c64b

Fix a backwards incompatibility in the new sync messages
Problem: some older clients were throwing an error when parsing sync
messages from sync protocol upgrades introduced in
d6c67f41e626493bf83612dc58c1829842ebe195. The problem is that the new
protocol was encoding changes using `crate::Change::bytes`, rather than
`crate::Change::raw_bytes`. The former compresses the change if it
larger than a certain threshold but the old sync protocol is not capable
of handling compressed changes.

Solution: use `raw_bytes` rather than `bytes` to avoid compressing
changes.

rust/automerge/src/sync.rs
@@ -229,15 +229,8 @@ impl SyncDoc for Automerge {
                 // deduplicate the changes to send with those we have already sent and clone it now
                 let changes = all_changes
                     .into_iter()
-                    .filter_map(|change| {
-                        if !sync_state.sent_hashes.contains(&change.hash()) {
-                            Some(change.clone())
-                        } else {
-                            None
-                        }
-                    })
-                    .collect::<Vec<_>>();
-                let hashes = changes.iter().map(|c| c.hash()).collect::<Vec<_>>();
+                    .filter(|change| !sync_state.sent_hashes.contains(&change.hash()));
+                let hashes = changes.clone().map(|c| c.hash()).collect::<Vec<_>>();
                 if sync_state.supports_v2_messages() {
                     let encoded = changes
                         .into_iter()
@@ -251,7 +244,7 @@ impl SyncDoc for Automerge {
         } else if sync_state.supports_v2_messages() {
             (MessageBuilder::new_v2(Vec::new()), Vec::new())
         } else {
-            (MessageBuilder::new_v1(Vec::new()), Vec::new())
+            (MessageBuilder::new_v1(std::iter::empty()), Vec::new())
         };
 
         let heads_unchanged = sync_state.last_sent_heads == our_heads;

rust/automerge/src/sync/message_builder.rs
@@ -12,15 +12,12 @@ pub(super) struct MessageBuilder {
 }
 
 impl MessageBuilder {
-    pub(super) fn new_v1(changes: Vec<Change>) -> Self {
+    pub(super) fn new_v1<'a, I: Iterator<Item = &'a Change>>(changes: I) -> Self {
         MessageBuilder {
             heads: Vec::new(),
             need: Vec::new(),
             have: Vec::new(),
-            changes: changes
-                .into_iter()
-                .map(|mut c| c.bytes().to_vec())
-                .collect(),
+            changes: changes.map(|c| c.raw_bytes().to_vec()).collect(),
             supported_capabilities: None,
             version: MessageVersion::V1,
         }

rust/automerge/src/sync/v1_compat_test/mod.rs
@@ -519,6 +519,37 @@ fn sync_from_v2_to_v1() {
     assert_eq!(doc1.get_heads(), doc2.get_heads());
 }
 
+#[test]
+fn sync_v1_to_v2_with_compressed_change() {
+    // Reproduce an issue where the v2 peer was sending changes as compressed bytes rather than
+    // uncompressed, which the old implementation couldn't handle.
+    let mut doc1 = AutoCommit::new();
+    let list = doc1.put_object(ROOT, "list", crate::ObjType::List).unwrap();
+    for index in 0..1000 {
+        doc1.insert(&list, index, index as i64).unwrap();
+    }
+    doc1.commit().unwrap();
+
+    let mut doc2 = AutoCommit::new();
+
+    let mut sync_state2 = crate::sync::State::new();
+    let mut sync_state1 = State::new();
+
+    sync_v1_to_v2(
+        &mut doc2.doc,
+        &mut doc1.doc,
+        &mut sync_state1,
+        &mut sync_state2,
+    );
+
+    assert_eq!(doc1.get_heads(), doc2.get_heads());
+
+    doc1.put(ROOT, "foo", "bar").unwrap();
+    doc2.put(ROOT, "baz", "quux").unwrap();
+    doc1.commit().unwrap();
+    doc2.commit().unwrap();
+}
+
 /// Run the sync protocol with the v1 peer starting first
 fn sync_v1_to_v2(
     v1: &mut crate::Automerge,
@@ -531,10 +562,7 @@ fn sync_v1_to_v2(
 
     loop {
         let a_to_b = v1.generate_sync_message_v1(a_sync_state);
-        let b_to_a = v2.generate_sync_message(b_sync_state);
-        if a_to_b.is_none() && b_to_a.is_none() {
-            break;
-        }
+        let a_to_b_is_none = a_to_b.is_none();
         if iterations > MAX_ITER {
             panic!("failed to sync in {} iterations", MAX_ITER);
         }
@@ -546,6 +574,8 @@ fn sync_v1_to_v2(
             tracing::debug!(decoded=?decoded, "receiving decoded message on v2");
             v2.receive_sync_message(b_sync_state, decoded).unwrap()
         }
+        let b_to_a = v2.generate_sync_message(b_sync_state);
+        let b_to_a_is_none = b_to_a.is_none();
         if let Some(msg) = b_to_a {
             tracing::debug!(msg=?msg, "sending message from v2 to v1");
             let encoded = msg.encode();
@@ -553,6 +583,9 @@ fn sync_v1_to_v2(
                 .expect("v1 message should decode as a v2 message");
             tracing::debug!(decoded=?decoded, "receiving decoded message on v1");
             v1.receive_sync_message_v1(a_sync_state, decoded).unwrap()
+        }
+        if a_to_b_is_none && b_to_a_is_none {
+            break;
         }
         iterations += 1;
     }
@@ -570,10 +603,7 @@ fn sync_v2_to_v1(
 
     loop {
         let a_to_b = v2.generate_sync_message(v2_sync_state);
-        let b_to_a = v1.generate_sync_message_v1(v1_sync_state);
-        if a_to_b.is_none() && b_to_a.is_none() {
-            break;
-        }
+        let a_to_b_is_none = a_to_b.is_none();
         if iterations > MAX_ITER {
             panic!("failed to sync in {} iterations", MAX_ITER);
         }
@@ -583,11 +613,16 @@ fn sync_v2_to_v1(
                 Message::decode(&encoded).expect("v1 message should decode as a v2 message");
             v1.receive_sync_message_v1(v1_sync_state, decoded).unwrap()
         }
+        let b_to_a = v1.generate_sync_message_v1(v1_sync_state);
+        let b_to_a_is_none = b_to_a.is_none();
         if let Some(msg) = b_to_a {
             let encoded = msg.encode();
             let decoded = crate::sync::Message::decode(&encoded)
                 .expect("v1 message should decode as a v2 message");
             v2.receive_sync_message(v2_sync_state, decoded).unwrap()
+        }
+        if a_to_b_is_none && b_to_a_is_none {
+            break;
         }
         iterations += 1;
     }