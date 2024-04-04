automerge/automerge

commit c17b6fb4d71b7bb65667e5f68db91a36d39bb133

rust: Add blocks related methods
This commit introduces the first components of rich text. To model rich
text in `ObjType::Text` objects we allow the insertion of
`ObjType::Map`s inline with the characters of the text. These maps
represent "block markers". This patch introduces the following new APIs
for working with block markers:

* Transactable::{split_block, join_block, update_block}
* ReadDoc::{spans, spans_at}

The first set of APIs offer ways to create, modify, and remove block
markers, whilst the second offer ways to view the state of the entire
text sequence.

rust/automerge/src/autocommit.rs
@@ -3,6 +3,7 @@ use std::ops::RangeBounds;
 use crate::automerge::SaveOptions;
 use crate::automerge::{current_state, diff};
 use crate::exid::ExId;
+use crate::iter::Spans;
 use crate::iter::{Keys, ListRange, MapRange, Values};
 use crate::marks::{ExpandMark, Mark, MarkSet};
 use crate::patches::{PatchLog, TextRepresentation};
@@ -741,6 +742,19 @@ impl ReadDoc for AutoCommit {
         self.doc.text_for(obj.as_ref(), self.get_scope(Some(heads)))
     }
 
+    fn spans<O: AsRef<ExId>>(&self, obj: O) -> Result<Spans<'_>, AutomergeError> {
+        self.doc.spans_for(obj.as_ref(), self.get_scope(None))
+    }
+
+    fn spans_at<O: AsRef<ExId>>(
+        &self,
+        obj: O,
+        heads: &[ChangeHash],
+    ) -> Result<Spans<'_>, AutomergeError> {
+        self.doc
+            .spans_for(obj.as_ref(), self.get_scope(Some(heads)))
+    }
+
     fn get_cursor<O: AsRef<ExId>>(
         &self,
         obj: O,
@@ -938,6 +952,30 @@ impl Transactable for AutoCommit {
             end,
             expand,
         )
+    }
+
+    fn split_block<'p, O>(&mut self, obj: O, index: usize) -> Result<ExId, AutomergeError>
+    where
+        O: AsRef<ExId>,
+    {
+        self.ensure_transaction_open();
+        let (patch_log, tx) = self.transaction.as_mut().unwrap();
+        tx.split_block(&mut self.doc, patch_log, obj.as_ref(), index)
+    }
+
+    fn join_block<O: AsRef<ExId>>(&mut self, text: O, index: usize) -> Result<(), AutomergeError> {
+        self.ensure_transaction_open();
+        let (patch_log, tx) = self.transaction.as_mut().unwrap();
+        tx.join_block(&mut self.doc, patch_log, text.as_ref(), index)
+    }
+
+    fn replace_block<'p, O>(&mut self, text: O, index: usize) -> Result<ExId, AutomergeError>
+    where
+        O: AsRef<ExId>,
+    {
+        self.ensure_transaction_open();
+        let (patch_log, tx) = self.transaction.as_mut().unwrap();
+        tx.replace_block(&mut self.doc, patch_log, text.as_ref(), index)
     }
 
     fn base_heads(&self) -> Vec<ChangeHash> {

rust/automerge/src/transaction/manual_transaction.rs
@@ -1,6 +1,7 @@
 use std::ops::RangeBounds;
 
 use crate::exid::ExId;
+use crate::iter::Spans;
 use crate::iter::{Keys, ListRange, MapRange, Values};
 use crate::marks::{ExpandMark, Mark, MarkSet};
 use crate::patches::PatchLog;
@@ -201,6 +202,19 @@ impl<'a> ReadDoc for Transaction<'a> {
         heads: &[ChangeHash],
     ) -> Result<String, AutomergeError> {
         self.doc.text_for(obj.as_ref(), self.get_scope(Some(heads)))
+    }
+
+    fn spans<O: AsRef<ExId>>(&self, obj: O) -> Result<Spans<'_>, AutomergeError> {
+        self.doc.spans_for(obj.as_ref(), self.get_scope(None))
+    }
+
+    fn spans_at<O: AsRef<ExId>>(
+        &self,
+        obj: O,
+        heads: &[ChangeHash],
+    ) -> Result<Spans<'_>, AutomergeError> {
+        self.doc
+            .spans_for(obj.as_ref(), self.get_scope(Some(heads)))
     }
 
     fn get_cursor<O: AsRef<ExId>>(
@@ -415,6 +429,27 @@ impl<'a> Transactable for Transaction<'a> {
         expand: ExpandMark,
     ) -> Result<(), AutomergeError> {
         self.do_tx(|tx, doc, hist| tx.unmark(doc, hist, obj.as_ref(), name, start, end, expand))
+    }
+
+    fn split_block<'p, O>(&mut self, obj: O, index: usize) -> Result<ExId, AutomergeError>
+    where
+        O: AsRef<ExId>,
+    {
+        self.do_tx(|tx, doc, hist| tx.split_block(doc, hist, obj.as_ref(), index))
+    }
+
+    fn join_block<O>(&mut self, text: O, index: usize) -> Result<(), AutomergeError>
+    where
+        O: AsRef<ExId>,
+    {
+        self.do_tx(|tx, doc, hist| tx.join_block(doc, hist, text.as_ref(), index))
+    }
+
+    fn replace_block<'p, O>(&mut self, text: O, index: usize) -> Result<ExId, AutomergeError>
+    where
+        O: AsRef<ExId>,
+    {
+        self.do_tx(|tx, doc, hist| tx.replace_block(doc, hist, text.as_ref(), index))
     }
 
     fn base_heads(&self) -> Vec<ChangeHash> {

rust/automerge/src/transaction/transactable.rs
@@ -110,6 +110,16 @@ pub trait Transactable: ReadDoc {
         expand: ExpandMark,
     ) -> Result<(), AutomergeError>;
 
+    fn split_block<O>(&mut self, obj: O, index: usize) -> Result<ExId, AutomergeError>
+    where
+        O: AsRef<ExId>;
+
+    fn join_block<O: AsRef<ExId>>(&mut self, text: O, index: usize) -> Result<(), AutomergeError>;
+
+    fn replace_block<O>(&mut self, text: O, index: usize) -> Result<ExId, AutomergeError>
+    where
+        O: AsRef<ExId>;
+
     /// The heads this transaction will be based on
     fn base_heads(&self) -> Vec<ChangeHash>;
 

rust/automerge-wasm/src/interop.rs
@@ -1349,7 +1349,6 @@ impl TryFrom<JsPatch> for JsValue {
             PatchAction::Insert {
                 index,
                 values,
-                marks,
                 ..
             } => {
                 let conflicts = values.iter().map(|v| v.2).collect::<Vec<_>>();
@@ -1372,17 +1371,6 @@ impl TryFrom<JsPatch> for JsValue {
                             .map(|c| JsValue::from(*c))
                             .collect::<Array>(),
                     )?;
-                }
-                if let Some(m) = marks {
-                    let marks = Object::new();
-                    for (name, value) in m.iter() {
-                        js_set(
-                            &marks,
-                            name,
-                            alloc(&value.into(), TextRepresentation::String).1,
-                        )?;
-                    }
-                    js_set(&result, "marks", marks)?;
                 }
                 Ok(result.into())
             }

commit 26a854c7debc1488eb9d7aa00698c144e7492410

Add Transactable::update_blocks
Problem: the block manipulation methods introduced in c17b6fb4d are
awkward to use with some text editors. The problem is that many text
editors expose a tree-structured data type, which is complicated to map
to the sequence of block markers and text which automerge users.

Solution: provide `Transactable::update_blocks`, which accepts a
sequence of `BlockOrText` objects and diffs these against the current
state of the text. This means that editors which provide drastically
different APIs need only provide a function which transforms the editor
state into the block list, rather than having to map the individual tree
mutations to block changes.

rust/automerge/src/autocommit.rs
@@ -1000,6 +1000,16 @@ impl Transactable for AutoCommit {
         crate::text_diff::myers_diff(&mut self.doc, tx, patch_log, obj, new_text)
     }
 
+    fn update_blocks<'a, O: AsRef<ExId>, I: IntoIterator<Item = crate::BlockOrText<'a>>>(
+        &mut self,
+        text: O,
+        new_text: I,
+    ) -> Result<(), AutomergeError> {
+        self.ensure_transaction_open();
+        let (patch_log, tx) = self.transaction.as_mut().unwrap();
+        crate::text_diff::myers_block_diff(&mut self.doc, tx, patch_log, text.as_ref(), new_text)
+    }
+
     fn update_object<O: AsRef<ExId>>(
         &mut self,
         obj: O,

rust/automerge/src/lib.rs
@@ -265,7 +265,7 @@ mod clock;
 mod columnar;
 mod convert;
 mod cursor;
-mod error;
+pub mod error;
 mod exid;
 pub mod hydrate;
 mod indexed_cache;
@@ -304,6 +304,7 @@ pub use patches::{Patch, PatchAction, PatchLog};
 pub use read::ReadDoc;
 pub use sequence_tree::SequenceTree;
 pub use storage::VerificationMode;
+pub use transaction::BlockOrText;
 pub use types::{ActorId, ChangeHash, ObjType, OpType, ParseChangeHashError, Prop};
 pub use value::{ScalarValue, Value};
 

rust/automerge/src/text_diff.rs
@@ -1,8 +1,12 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 use crate::{
-    text_value::TextValue, transaction::TransactionInner, Automerge, ObjId as ExId, PatchLog,
-    ReadDoc,
+    clock::Clock,
+    iter::{SpanInternal, SpansInternal},
+    op_tree::OpTreeOpIter,
+    text_value::TextValue,
+    transaction::TransactionInner,
+    Automerge, BlockOrText, ObjId as ExId, PatchLog, ReadDoc,
 };
 mod myers;
 mod replace;
@@ -124,4 +128,307 @@ impl<'a> myers::DiffHook for TxHook<'a> {
         self.idx += TextValue::width(&new_chars);
         Ok(())
     }
+}
+
+pub(crate) fn myers_block_diff<'a, 'b, I: IntoIterator<Item = BlockOrText<'b>>>(
+    doc: &'a mut Automerge,
+    tx: &'a mut TransactionInner,
+    patch_log: &mut PatchLog,
+    text_obj: &crate::ObjId,
+    new: I,
+) -> Result<(), crate::AutomergeError> {
+    let text_obj_meta = doc.exid_to_obj(text_obj)?;
+    let old = spans_as_grapheme(doc, &text_obj_meta.id, None)?;
+    let new = block_or_text_as_grapheme(new.into_iter());
+    let mut hook = replace::Replace::new(BlockDiffHook {
+        tx,
+        doc,
+        patch_log,
+        obj: text_obj,
+        idx: 0,
+        old: &old,
+        new: &new,
+    });
+    //let mut hook = BlockDiffHook {
+    //tx,
+    //doc,
+    //patch_log,
+    //obj: text_obj,
+    //idx: 0,
+    //old: &old,
+    //new: &new,
+    //};
+    myers::diff(&mut hook, &old, 0..old.len(), &new, 0..new.len())
+}
+
+struct BlockDiffHook<'a> {
+    doc: &'a mut Automerge,
+    tx: &'a mut TransactionInner,
+    patch_log: &'a mut PatchLog,
+    old: &'a [BlockOrGrapheme],
+    new: &'a [BlockOrGrapheme],
+    obj: &'a ExId,
+    idx: usize,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum BlockOrGrapheme {
+    Block(crate::hydrate::Map),
+    Grapheme(String),
+}
+
+impl BlockOrGrapheme {
+    fn width(&self) -> usize {
+        match self {
+            BlockOrGrapheme::Block(_) => 1,
+            BlockOrGrapheme::Grapheme(g) => TextValue::width(g),
+        }
+    }
+}
+
+impl<'a> myers::DiffHook for BlockDiffHook<'a> {
+    type Error = crate::AutomergeError;
+
+    fn equal(
+        &mut self,
+        old_index: usize,
+        _new_index: usize,
+        len: usize,
+    ) -> Result<(), Self::Error> {
+        for i in 0..len {
+            self.idx += self.old[old_index + i].width();
+        }
+        Ok(())
+    }
+
+    fn delete(
+        &mut self,
+        old_index: usize,
+        old_len: usize,
+        _new_index: usize,
+    ) -> Result<(), Self::Error> {
+        for i in old_index..old_index + old_len {
+            match &self.old[i] {
+                BlockOrGrapheme::Block(_) => {
+                    self.tx
+                        .join_block(self.doc, self.patch_log, self.obj, self.idx)?;
+                }
+                BlockOrGrapheme::Grapheme(_) => {
+                    self.tx
+                        .delete(self.doc, self.patch_log, self.obj, self.idx)?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn insert(
+        &mut self,
+        _old_index: usize,
+        new_index: usize,
+        new_len: usize,
+    ) -> Result<(), Self::Error> {
+        //accumulate runs of text and insert them. Insert blocks immediately
+        let mut run = String::new();
+        for i in new_index..new_index + new_len {
+            match &self.new[i] {
+                BlockOrGrapheme::Block(b) => {
+                    if !run.is_empty() {
+                        self.tx.splice_text(
+                            self.doc,
+                            self.patch_log,
+                            self.obj,
+                            self.idx,
+                            0,
+                            &run,
+                        )?;
+                        self.idx += TextValue::width(&run);
+                        run.clear();
+                    }
+                    split_block(self.doc, self.tx, self.patch_log, self.obj, self.idx, b)?;
+                    self.idx += 1;
+                }
+                BlockOrGrapheme::Grapheme(g) => {
+                    run.push_str(g);
+                }
+            }
+        }
+        if !run.is_empty() {
+            self.tx
+                .splice_text(self.doc, self.patch_log, self.obj, self.idx, 0, &run)?;
+            self.idx += TextValue::width(&run);
+        }
+        Ok(())
+    }
+
+    fn replace(
+        &mut self,
+        old_index: usize,
+        old_len: usize,
+        new_index: usize,
+        new_len: usize,
+    ) -> Result<(), Self::Error> {
+        // iterate through the old and new indices, if we're replacing a block with a block, update
+        // the block. Otherwise, delete the old and insert the new
+        let mut old_idx = old_index;
+        let mut new_idx = new_index;
+        while old_idx < old_index + old_len || new_idx < new_index + new_len {
+            let old = if old_idx < old_index + old_len {
+                self.old.get(old_idx)
+            } else {
+                None
+            };
+            let new = if new_idx < new_index + new_len {
+                self.new.get(new_idx)
+            } else {
+                None
+            };
+            match (old, new) {
+                (None, None) => {}
+                (None, Some(val)) => match val {
+                    BlockOrGrapheme::Block(b) => {
+                        split_block(self.doc, self.tx, self.patch_log, self.obj, self.idx, b)?;
+                        self.idx += 1;
+                        new_idx += 1;
+                    }
+                    BlockOrGrapheme::Grapheme(g) => {
+                        self.tx
+                            .splice_text(self.doc, self.patch_log, self.obj, self.idx, 0, g)?;
+                        self.idx += TextValue::width(g);
+                        new_idx += 1;
+                    }
+                },
+                (Some(val), None) => match val {
+                    BlockOrGrapheme::Block(_) => {
+                        self.tx
+                            .join_block(self.doc, self.patch_log, self.obj, self.idx)?;
+                        old_idx += 1;
+                    }
+                    BlockOrGrapheme::Grapheme(_) => {
+                        self.tx
+                            .delete(self.doc, self.patch_log, self.obj, self.idx)?;
+                        old_idx += 1;
+                    }
+                },
+                (Some(old), Some(new)) => match (old, new) {
+                    (BlockOrGrapheme::Block(b1), BlockOrGrapheme::Block(b2)) => {
+                        if b1 != b2 {
+                            update_block(self.doc, self.tx, self.patch_log, self.obj, self.idx, b2)?
+                        }
+                        self.idx += 1;
+                        old_idx += 1;
+                        new_idx += 1;
+                    }
+                    (BlockOrGrapheme::Grapheme(_g1), BlockOrGrapheme::Grapheme(g2)) => {
+                        self.tx
+                            .delete(self.doc, self.patch_log, self.obj, self.idx)?;
+                        self.tx
+                            .splice_text(self.doc, self.patch_log, self.obj, self.idx, 0, g2)?;
+                        self.idx += TextValue::width(g2);
+                        old_idx += 1;
+                        new_idx += 1;
+                    }
+                    (BlockOrGrapheme::Block(_), BlockOrGrapheme::Grapheme(g2)) => {
+                        self.tx
+                            .join_block(self.doc, self.patch_log, self.obj, self.idx)?;
+                        self.tx
+                            .splice_text(self.doc, self.patch_log, self.obj, self.idx, 0, g2)?;
+                        self.idx += TextValue::width(g2);
+                        old_idx += 1;
+                        new_idx += 1;
+                    }
+                    (BlockOrGrapheme::Grapheme(_g1), BlockOrGrapheme::Block(b2)) => {
+                        self.tx
+                            .delete(self.doc, self.patch_log, self.obj, self.idx)?;
+                        split_block(self.doc, self.tx, self.patch_log, self.obj, self.idx, b2)?;
+                        self.idx += 1;
+                        old_idx += 1;
+                        new_idx += 1;
+                    }
+                },
+            }
+        }
+
+        Ok(())
+    }
+
+    fn finish(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+fn spans_as_grapheme(
+    doc: &Automerge,
+    text: &crate::types::ObjId,
+    clock: Option<Clock>,
+) -> Result<Vec<BlockOrGrapheme>, crate::AutomergeError> {
+    let spans_internal = SpansInternal::new(
+        OpTreeOpIter::new(doc.ops().iter_obj(text).unwrap(), doc.osd()),
+        doc,
+        clock.clone(),
+    );
+    let mut result = Vec::with_capacity(spans_internal.size_hint().0);
+    for span in spans_internal {
+        match span {
+            SpanInternal::Obj(b, _) => {
+                let crate::hydrate::Value::Map(map) = doc.hydrate_map(&b.into(), clock.as_ref())
+                else {
+                    tracing::warn!("unexpected non map object in text");
+                    result.push(BlockOrGrapheme::Block(crate::hydrate::Map::new()));
+                    continue;
+                };
+                result.push(BlockOrGrapheme::Block(map));
+            }
+            SpanInternal::Text(t, _, _) => {
+                for g in t.graphemes(true) {
+                    result.push(BlockOrGrapheme::Grapheme(g.to_string()));
+                }
+            }
+        }
+    }
+    Ok(result)
+}
+
+fn block_or_text_as_grapheme<'a, I: Iterator<Item = BlockOrText<'a>>>(
+    iter: I,
+) -> Vec<BlockOrGrapheme> {
+    let mut result = Vec::with_capacity(iter.size_hint().0);
+    for b in iter {
+        match b {
+            BlockOrText::Block(b) => result.push(BlockOrGrapheme::Block(b)),
+            BlockOrText::Text(t) => {
+                for g in t.graphemes(true) {
+                    result.push(BlockOrGrapheme::Grapheme(g.to_string()));
+                }
+            }
+        }
+    }
+    result
+}
+
+fn split_block(
+    doc: &mut Automerge,
+    tx: &mut TransactionInner,
+    patch_log: &mut PatchLog,
+    obj: &crate::ObjId,
+    index: usize,
+    block: &crate::hydrate::Map,
+) -> Result<(), crate::error::AutomergeError> {
+    let new_block = tx.split_block(doc, patch_log, obj, index)?;
+    tx.update_map(doc, patch_log, &new_block, block)
+}
+
+fn update_block(
+    doc: &mut Automerge,
+    tx: &mut TransactionInner,
+    patch_log: &mut PatchLog,
+    obj: &crate::ObjId,
+    index: usize,
+    new_block: &crate::hydrate::Map,
+) -> Result<(), crate::error::AutomergeError> {
+    let Some((crate::Value::Object(crate::ObjType::Map), block_id)) = doc.get(obj, index)? else {
+        return Err(crate::error::AutomergeError::InvalidIndex(index));
+    };
+
+    tx.update_map(doc, patch_log, &block_id, new_block)
 }

rust/automerge/src/transaction.rs
@@ -5,7 +5,7 @@ mod result;
 mod transactable;
 
 pub use self::commit::CommitOptions;
-pub use self::transactable::Transactable;
+pub use self::transactable::{BlockOrText, Transactable};
 pub(crate) use inner::{TransactionArgs, TransactionInner};
 pub use manual_transaction::Transaction;
 pub use result::Failure;

rust/automerge/src/transaction/manual_transaction.rs
@@ -475,6 +475,16 @@ impl<'a> Transactable for Transaction<'a> {
         self.do_tx(|tx, doc, hist| crate::text_diff::myers_diff(doc, tx, hist, obj, new_text))
     }
 
+    fn update_blocks<'b, O: AsRef<ExId>, I: IntoIterator<Item = crate::BlockOrText<'b>>>(
+        &mut self,
+        text: O,
+        new_text: I,
+    ) -> Result<(), AutomergeError> {
+        self.do_tx(move |tx, doc, hist| {
+            crate::text_diff::myers_block_diff(doc, tx, hist, text.as_ref(), new_text)
+        })
+    }
+
     fn update_object<O: AsRef<ExId>>(
         &mut self,
         obj: O,

rust/automerge/src/transaction/transactable.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use crate::exid::ExId;
 use crate::marks::{ExpandMark, Mark};
 use crate::{AutomergeError, ChangeHash, ObjType, Prop, ReadDoc, ScalarValue};
@@ -120,6 +122,12 @@ pub trait Transactable: ReadDoc {
     where
         O: AsRef<ExId>;
 
+    fn update_blocks<'a, O: AsRef<ExId>, I: IntoIterator<Item = BlockOrText<'a>>>(
+        &mut self,
+        text: O,
+        new_text: I,
+    ) -> Result<(), AutomergeError>;
+
     /// The heads this transaction will be based on
     fn base_heads(&self) -> Vec<ChangeHash>;
 
@@ -138,4 +146,10 @@ pub trait Transactable: ReadDoc {
         obj: O,
         new_value: &crate::hydrate::Value,
     ) -> Result<(), crate::error::UpdateObjectError>;
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum BlockOrText<'a> {
+    Block(crate::hydrate::Map),
+    Text(Cow<'a, str>),
 }

rust/automerge/tests/block_tests.rs
@@ -1,0 +1,255 @@
+use automerge::{
+    hydrate_list, hydrate_map, hydrate_text, transaction::Transactable, BlockOrText, ObjType,
+    ReadDoc, ROOT,
+};
+use test_log::test;
+
+#[test]
+fn update_blocks_change_block_properties() {
+    let mut doc = automerge::AutoCommit::new();
+    let text = doc.put_object(ROOT, "text", ObjType::Text).unwrap();
+    let block1 = doc.split_block(&text, 0).unwrap();
+    doc.update_object(
+        &block1,
+        &hydrate_map! {
+            "parents" => hydrate_list![],
+            "type" => "ordered-list-item",
+            "attrs" => hydrate_map!{}
+        }
+        .into(),
+    )
+    .unwrap();
+    doc.splice_text(&text, 1, 0, "item 1").unwrap();
+    let block2 = doc.split_block(&text, 7).unwrap();
+    doc.update_object(
+        &block2,
+        &hydrate_map! {
+            "parents" => hydrate_list![],
+            "type" => "ordered-list-item",
+            "attrs" => hydrate_map!{}
+        }
+        .into(),
+    )
+    .unwrap();
+    doc.splice_text(&text, 8, 0, "item 2").unwrap();
+
+    doc.update_diff_cursor();
+
+    doc.update_blocks(
+        &text,
+        [
+            BlockOrText::Block(hydrate_map! {
+                "type" => "paragraph",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("item 1".into()),
+            BlockOrText::Block(hydrate_map! {
+                "type" => "unordered-list-item",
+                "parents" => hydrate_list!["ordered-list-item"],
+                "attrs" => hydrate_map!{
+                    "key" => 1,
+                },
+            }),
+            BlockOrText::Text("item 2".into()),
+        ],
+    )
+    .unwrap();
+
+    let spans = doc
+        .spans(&text)
+        .unwrap()
+        .map(|s| match s {
+            automerge::iter::Span::Block(b) => BlockOrText::Block(b),
+            automerge::iter::Span::Text(t, _) => BlockOrText::Text(std::borrow::Cow::Owned(t)),
+        })
+        .collect::<Vec<_>>();
+    assert_eq!(
+        spans,
+        vec![
+            BlockOrText::Block(hydrate_map! {
+                "type" => "paragraph",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("item 1".into()),
+            BlockOrText::Block(hydrate_map! {
+                "type" => "unordered-list-item",
+                "parents" => hydrate_list!["ordered-list-item"],
+                "attrs" => hydrate_map!{"key" => 1}
+            }),
+            BlockOrText::Text("item 2".into()),
+        ]
+    );
+}
+
+#[test]
+fn update_blocks_updates_text() {
+    let mut doc = automerge::AutoCommit::new();
+    let text = doc.put_object(ROOT, "text", ObjType::Text).unwrap();
+    let _block1 = doc.split_block(&text, 0).unwrap();
+    doc.splice_text(&text, 1, 0, "first thing").unwrap();
+    let _block2 = doc.split_block(&text, 12).unwrap();
+    doc.splice_text(&text, 13, 0, "second thing").unwrap();
+
+    doc.update_diff_cursor();
+
+    doc.update_blocks(
+        &text,
+        [
+            BlockOrText::Block(hydrate_map! {
+                "type" => "ordered-list-item",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("the first thing".into()),
+            BlockOrText::Block(hydrate_map! {
+                "type" => "paragraph",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("the things are done".into()),
+        ],
+    )
+    .unwrap();
+
+    //let spans = doc.spans(&text).unwrap().collect::<Vec<_>>();
+    let spans = doc
+        .spans(&text)
+        .unwrap()
+        .map(|s| match s {
+            automerge::iter::Span::Block(b) => BlockOrText::Block(b),
+            automerge::iter::Span::Text(t, _) => BlockOrText::Text(std::borrow::Cow::Owned(t)),
+        })
+        .collect::<Vec<_>>();
+    assert_eq!(
+        spans,
+        vec![
+            BlockOrText::Block(hydrate_map! {
+                "type" => "ordered-list-item",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("the first thing".into()),
+            BlockOrText::Block(hydrate_map! {
+                "type" => "paragraph",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("the things are done".into()),
+        ]
+    );
+}
+
+#[test]
+fn update_blocks_noop() {
+    let mut doc = automerge::AutoCommit::new();
+    let text = doc.put_object(ROOT, "text", ObjType::Text).unwrap();
+    let block1 = doc.split_block(&text, 0).unwrap();
+    doc.update_object(
+        &block1,
+        &hydrate_map! {
+            "parents" => hydrate_list![],
+            "type" => "ordered-list-item",
+            "attrs" => hydrate_map!{}
+        }
+        .into(),
+    )
+    .unwrap();
+    doc.splice_text(&text, 1, 0, "item 1").unwrap();
+
+    doc.update_diff_cursor();
+
+    doc.update_blocks(
+        &text,
+        [
+            BlockOrText::Block(hydrate_map! {
+                "type" => "ordered-list-item",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("item 1".into()),
+        ],
+    )
+    .unwrap();
+
+    let patches = doc.diff_incremental();
+    assert_eq!(patches.len(), 0, "expected no patches");
+}
+
+#[test]
+fn update_blocks_updates_text_and_blocks_at_once() {
+    let mut doc = automerge::AutoCommit::new();
+    let text = doc.put_object(ROOT, "text", ObjType::Text).unwrap();
+    //let block1 = doc.split_block(&text, 0, NewBlock::new("paragraph"))
+    let block1 = doc.split_block(&text, 0).unwrap();
+    doc.update_object(
+        &block1,
+        &hydrate_map! {
+            "parents" => hydrate_list![],
+            "type" => "paragraph",
+            "attrs" => hydrate_map!{}
+        }
+        .into(),
+    )
+    .unwrap();
+    doc.splice_text(&text, 1, 0, "hello world").unwrap();
+
+    doc.update_blocks(
+        &text,
+        vec![
+            BlockOrText::Block(hydrate_map! {
+                "type" => "unordered-list-item",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            BlockOrText::Text("goodbye world".into()),
+        ],
+    )
+    .unwrap();
+
+    let spans_after = doc.spans(&text).unwrap().collect::<Vec<_>>();
+    assert_eq!(
+        spans_after,
+        vec![
+            automerge::iter::Span::Block(hydrate_map! {
+                "type" => "unordered-list-item",
+                "parents" => hydrate_list![],
+                "attrs" => hydrate_map!{}
+            }),
+            automerge::iter::Span::Text("goodbye world".into(), None),
+        ]
+    );
+}
+
+#[test]
+fn text_complex_block_properties() {
+    let mut doc = automerge::AutoCommit::new();
+    let text = doc.put_object(ROOT, "text", ObjType::Text).unwrap();
+    //let block = doc.split_block(&text, 0, NewBlock::new("ordered-list-item"))
+    //.unwrap();
+    let block1 = doc.split_block(&text, 0).unwrap();
+    doc.update_object(
+        &block1,
+        &hydrate_map! {
+            "type" => hydrate_text!("ordered-list-item"),
+            "parents" => hydrate_list![hydrate_text!("div")],
+        }
+        .into(),
+    )
+    .unwrap();
+
+    let (text_obj, text_id) = doc.get(&block1, "type").unwrap().unwrap();
+    assert_eq!(text_obj, automerge::Value::Object(automerge::ObjType::Text));
+    let value = doc.text(text_id).unwrap();
+    assert_eq!(value, "ordered-list-item");
+
+    let (list_obj, list_id) = doc.get(&block1, "parents").unwrap().unwrap();
+    assert_eq!(list_obj, automerge::Value::Object(automerge::ObjType::List));
+    let len = doc.length(&list_id);
+    assert_eq!(len, 1);
+    let (elem, elem_id) = doc.get(&list_id, 0).unwrap().unwrap();
+    assert_eq!(elem, automerge::Value::Object(automerge::ObjType::Text));
+    let elem_text = doc.text(elem_id).unwrap();
+    assert_eq!(elem_text, "div");
+}