0ffa67281802b76296f57de116042de42b7d5c90
actions <actions@github.com>
2024-04-09 10:00:51 +0000

    Add deno js release files

---
+++ deno_js_dist/raw_string.ts
@@ -0,0 +1,13 @@
+export class RawString {
+  val: string
+  constructor(val: string) {
+    this.val = val
+  }
+
+  /**
+   * Returns the content of the RawString object as a simple string
+   */
+  toString(): string {
+    return this.val
+  }
+}
