93241d782d754a4532f37fd7b933dd46dd38d6f2
Peter van Hardenberg <pvh@pvh.ca>
2024-09-22 19:18:47 -0700

    added moby-dick

--- rust/automerge/benches/bestiary.rs
+++ rust/automerge/benches/bestiary.rs
@@ -1,17 +1,22 @@
-use automerge::{transaction::Transactable, Automerge, ObjType, ROOT};
+use automerge::Automerge;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn load_save(data: &Vec<u8>) {
+
+fn bench_file(c: &mut Criterion, filename: &str) {
+    let data =  std::fs::read(filename).unwrap();
+    c.bench_function(&format!("bestiary_load {}", filename), |b| {
+        b.iter(|| Automerge::load(data.as_slice()))
+    });
+
     let doc = Automerge::load(data.as_slice()).unwrap();
-    let _save_data = doc.save();
+    c.bench_function(&format!("bestiary_save {}", filename), |b| {
+        b.iter(|| doc.save())
+    });
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let filename = "./benches/embark.automerge";
-    let data =  std::fs::read(filename).unwrap();
-    c.bench_function(&format!("load_save_embark {}", filename), |b| {
-        b.iter(|| load_save(black_box(&data)))
-    });
+    bench_file(c, "./benches/embark.automerge");
+    bench_file(c, "./benches/moby-dick.automerge");
 }
 
 criterion_group!(benches, criterion_benchmark);
