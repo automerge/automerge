19ade709974054b1e2f69543247f8ffb9726a191
Orion Henry <orion.henry@gmail.com>
2024-10-04 12:16:28 -0500

    move packer benchmarks to criterion

--- rust/packer/benches/insert.rs
+++ rust/packer/benches/insert.rs
@@ -1,70 +1,38 @@
-/*
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use packer::*;
 use rand::prelude::*;
-use test::Bencher;
 
 const MIN: u64 = 1;
+const MAX: u64 = u32::MAX as u64;
 
-#[bench]
-fn insert_10_large(b: &mut Bencher) {
-    insert_n(b, 10, u32::MAX as u64);
-}
-
-#[bench]
-fn insert_10_small(b: &mut Bencher) {
-    insert_n(b, 10, MIN);
-}
-
-#[bench]
-fn insert_100_large(b: &mut Bencher) {
-    insert_n(b, 100, u32::MAX as u64);
-}
-
-#[bench]
-fn insert_100_small(b: &mut Bencher) {
-    insert_n(b, 100, MIN);
-}
-
-#[bench]
-fn insert_1000_large(b: &mut Bencher) {
-    insert_n(b, 1000, u32::MAX as u64);
-}
-
-#[bench]
-fn insert_1000_small(b: &mut Bencher) {
-    insert_n(b, 1000, MIN);
-}
-
-#[bench]
-fn insert_10000_large(b: &mut Bencher) {
-    insert_n(b, 10000, u32::MAX as u64);
-}
-
-#[bench]
-fn insert_10000_small(b: &mut Bencher) {
-    insert_n(b, 10000, MIN);
-}
-
-fn insert_n(b: &mut Bencher, n: usize, max: u64) {
+fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
-    let mut col: ColumnData<IntCursor> = ColumnData::new();
-    for _ in 0..n {
-        let val: u64 = rng.gen();
-        let val = val % max;
-        let pos: usize = rng.gen();
-        let pos = pos % (col.len() + 1);
-        col.splice(pos, 0, vec![val as u64]);
+    for n in vec![10, 100, 1_000, 10_000] {
+        c.bench_function(&format!("insert_{}_large", n), |b| {
+            let mut col = gen_col(&mut rng, n, MAX);
+            b.iter(|| insert(&mut rng, black_box(&mut col), MAX))
+        });
+        c.bench_function(&format!("insert_{}_small", n), |b| {
+            let mut col = gen_col(&mut rng, n, MIN);
+            b.iter(|| insert(&mut rng, black_box(&mut col), MIN))
+        });
     }
-    b.iter(|| {
-        let val: u64 = rng.gen();
-        let val = val % max;
-        let pos: usize = rng.gen();
-        let pos = pos % (col.len() + 1);
-        col.splice(pos, 0, vec![val as u64]);
-    });
 }
-*/
+
+fn gen_col(rng: &mut ThreadRng, n: usize, max: u64) -> ColumnData<IntCursor> {
+    let mut col: ColumnData<IntCursor> = ColumnData::new();
+    let values: Vec<u64> = (0..n).map(|_| rng.gen::<u64>() % max).collect();
+    col.splice(0, 0, values);
+    col
+}
+
+fn insert(rng: &mut ThreadRng, col: &mut ColumnData<IntCursor>, max: u64) {
+    let val: u64 = rng.gen();
+    let val = val % max;
+    let pos: usize = rng.gen();
+    let pos = pos % (col.len() + 1);
+    col.splice(pos, 0, vec![val as u64]);
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- rust/packer/benches/seek.rs
+++ rust/packer/benches/seek.rs
@@ -1,67 +1,35 @@
-/*
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use packer::*;
 use rand::prelude::*;
-use test::Bencher;
 
 const MIN: u64 = 1;
+const MAX: u64 = u32::MAX as u64;
 
-#[bench]
-fn seek_10_large(b: &mut Bencher) {
-    seek_n(b, 10, u32::MAX as u64);
-}
-
-#[bench]
-fn seek_10_small(b: &mut Bencher) {
-    seek_n(b, 10, MIN);
-}
-
-#[bench]
-fn seek_100_large(b: &mut Bencher) {
-    seek_n(b, 100, u32::MAX as u64);
-}
-
-#[bench]
-fn seek_100_small(b: &mut Bencher) {
-    seek_n(b, 100, MIN);
-}
-
-#[bench]
-fn seek_1000_large(b: &mut Bencher) {
-    seek_n(b, 1000, u32::MAX as u64);
-}
-
-#[bench]
-fn seek_1000_small(b: &mut Bencher) {
-    seek_n(b, 1000, MIN);
-}
-
-#[bench]
-fn seek_10000_large(b: &mut Bencher) {
-    seek_n(b, 10000, u32::MAX as u64);
-}
-
-#[bench]
-fn seek_10000_small(b: &mut Bencher) {
-    seek_n(b, 10000, MIN);
-}
-
-fn seek_n(b: &mut Bencher, n: usize, max: u64) {
+fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
+    for n in vec![10, 100, 1_000, 10_000] {
+        c.bench_function(&format!("seek_{}_large", n), |b| {
+            let mut col = gen_col(&mut rng, n, MAX);
+            b.iter(|| seek(&mut rng, black_box(&mut col)))
+        });
+        c.bench_function(&format!("seek_{}_small", n), |b| {
+            let mut col = gen_col(&mut rng, n, MIN);
+            b.iter(|| seek(&mut rng, black_box(&mut col)))
+        });
+    }
+}
+
+fn gen_col(rng: &mut ThreadRng, n: usize, max: u64) -> ColumnData<IntCursor> {
     let mut col: ColumnData<IntCursor> = ColumnData::new();
     let values: Vec<u64> = (0..n).map(|_| rng.gen::<u64>() % max).collect();
     col.splice(0, 0, values);
-    println!(
-        "COL bytes={} vs {}",
-        col.byte_len(),
-        n * std::mem::size_of::<u64>()
-    );
-    b.iter(|| {
-        let pos = rng.gen::<usize>() % col.len();
-        col.get(pos);
-    });
+    col
 }
-*/
+
+fn seek(rng: &mut ThreadRng, col: &mut ColumnData<IntCursor>) {
+    let pos = rng.gen::<usize>() % col.len();
+    col.get(pos);
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
