6eae3d8f8f11298c260ead0d70799525362c1cd4
matt <5974590+mattheson@users.noreply.github.com>
2024-12-19 16:11:37 -0800

    `seek_list_op()` test

--- rust/automerge/src/automerge/tests.rs
+++ rust/automerge/src/automerge/tests.rs
@@ -246,8 +246,12 @@ fn test_cursors() -> Result<(), AutomergeError> {
     tx.splice_text(&text2, 0, 0, "aaa@bbb").unwrap();
     tx.commit();
 
-    let before_cursor = doc.get_cursor_moving(&text2, 3, None, MoveCursor::Before).unwrap();
-    let after_cursor = doc.get_cursor_moving(&text2, 3, None, MoveCursor::After).unwrap();
+    let before_cursor = doc
+        .get_cursor_moving(&text2, 3, None, MoveCursor::Before)
+        .unwrap();
+    let after_cursor = doc
+        .get_cursor_moving(&text2, 3, None, MoveCursor::After)
+        .unwrap();
     let start_cursor = doc.get_cursor(&text2, CursorPosition::Start, None).unwrap();
     let end_cursor = doc.get_cursor(&text2, CursorPosition::End, None).unwrap();
 
@@ -256,10 +260,18 @@ fn test_cursors() -> Result<(), AutomergeError> {
     // ^  ^   ^
     // s  ba  e
 
-    let pos_before = doc.get_cursor_position(&text2, &before_cursor, None).unwrap();
-    let pos_after = doc.get_cursor_position(&text2, &after_cursor, None).unwrap();
-    let pos_start = doc.get_cursor_position_for(&text2, &start_cursor, None).unwrap();
-    let pos_end = doc.get_cursor_position_for(&text2, &end_cursor, None).unwrap();
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
 
     assert_eq!(pos_before, 3);
     assert_eq!(pos_after, 3);
@@ -275,10 +287,18 @@ fn test_cursors() -> Result<(), AutomergeError> {
     // ^ ^   ^  ^
     // s b   a  e
 
-    let pos_before = doc.get_cursor_position(&text2, &before_cursor, None).unwrap();
-    let pos_after = doc.get_cursor_position(&text2, &after_cursor, None).unwrap();
-    let pos_start = doc.get_cursor_position_for(&text2, &start_cursor, None).unwrap();
-    let pos_end = doc.get_cursor_position_for(&text2, &end_cursor, None).unwrap();
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
 
     assert_eq!(pos_before, 2);
     assert_eq!(pos_after, 6);
@@ -294,10 +314,18 @@ fn test_cursors() -> Result<(), AutomergeError> {
     // ^  ^
     // sba e
 
-    let pos_before = doc.get_cursor_position(&text2, &before_cursor, None).unwrap();
-    let pos_after = doc.get_cursor_position(&text2, &after_cursor, None).unwrap();
-    let pos_start = doc.get_cursor_position_for(&text2, &start_cursor, None).unwrap();
-    let pos_end = doc.get_cursor_position_for(&text2, &end_cursor, None).unwrap();
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
 
     assert_eq!(pos_before, 0);
     assert_eq!(pos_after, 0);
@@ -313,10 +341,18 @@ fn test_cursors() -> Result<(), AutomergeError> {
     // ^    ^  ^
     // sb   a  e
 
-    let pos_before = doc.get_cursor_position(&text2, &before_cursor, None).unwrap();
-    let pos_after = doc.get_cursor_position(&text2, &after_cursor, None).unwrap();
-    let pos_start = doc.get_cursor_position_for(&text2, &start_cursor, None).unwrap();
-    let pos_end = doc.get_cursor_position_for(&text2, &end_cursor, None).unwrap();
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
 
     assert_eq!(pos_before, 0);
     assert_eq!(pos_after, 5);
@@ -334,10 +370,18 @@ fn test_cursors() -> Result<(), AutomergeError> {
     // ^         ^  ^
     // sb        a  e
 
-    let pos_before = doc.get_cursor_position(&text2, &before_cursor, None).unwrap();
-    let pos_after = doc.get_cursor_position(&text2, &after_cursor, None).unwrap();
-    let pos_start = doc.get_cursor_position_for(&text2, &start_cursor, None).unwrap();
-    let pos_end = doc.get_cursor_position_for(&text2, &end_cursor, None).unwrap();
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
 
     assert_eq!(pos_before, 0);
     assert_eq!(pos_after, 10);
@@ -349,21 +393,100 @@ fn test_cursors() -> Result<(), AutomergeError> {
     tx.splice_text(&text2, 5, 8, "").unwrap();
     tx.commit();
 
-    println!("{}", serde_json::to_string_pretty(&AutoSerde::from(&doc)).unwrap());
+    // println!(
+    //     "{}",
+    //     serde_json::to_string_pretty(&AutoSerde::from(&doc)).unwrap()
+    // );
 
     // hello
     // ^    ^
     // sb   ae
 
-    let pos_before = doc.get_cursor_position(&text2, &before_cursor, None).unwrap();
-    let pos_after = doc.get_cursor_position(&text2, &after_cursor, None).unwrap();
-    let pos_start = doc.get_cursor_position_for(&text2, &start_cursor, None).unwrap();
-    let pos_end = doc.get_cursor_position_for(&text2, &end_cursor, None).unwrap();
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
 
     assert_eq!(pos_before, 0);
     assert_eq!(pos_after, 5);
     assert_eq!(pos_start, 0);
     assert_eq!(pos_end, 5);
+
+    // test for semantics of `MoveCursor::After`/`seek_list_op()`
+    // -----------------------------------------------------------------------------------------
+    // setup for test:
+    let mut tx = doc.transaction();
+    let text2 = tx.put_object(ROOT, "hi", ObjType::Text).unwrap();
+    tx.splice_text(&text2, 0, 0, "aaa@bbb").unwrap();
+    tx.commit();
+
+    let before_cursor = doc
+        .get_cursor_moving(&text2, 3, None, MoveCursor::Before)
+        .unwrap();
+    let after_cursor = doc
+        .get_cursor_moving(&text2, 3, None, MoveCursor::After)
+        .unwrap();
+    let start_cursor = doc.get_cursor(&text2, CursorPosition::Start, None).unwrap();
+    let end_cursor = doc.get_cursor(&text2, CursorPosition::End, None).unwrap();
+
+    // setup:
+    // aaa@bbb
+    // ^  ^   ^
+    // s  ba  e
+
+    // beginning of semantic test: ----------------------------------------------
+
+    // remove "@bb", note that we've left one 'b'
+
+    let mut tx = doc.transaction();
+    tx.splice_text(&text2, 3, 3, "!!!").unwrap();
+    tx.commit();
+
+    println!(
+        "{}",
+        serde_json::to_string_pretty(&AutoSerde::from(&doc)).unwrap()
+    );
+
+    // correct semantics (current behavior)
+    // aaa!!!b
+    // ^ ^   ^ ^
+    // s b   a e
+    // (notice how the `after` cursor moves to the 'b' which is correct; it's the next visible item that was also visible at cursor creation)
+    // (also note that the `after` cursor is JUST returning the index found by `seek_list_op()`)
+
+    // alex:
+    // if `seek_list_op()` just returned the index of the first visible item (regardless if it was visible at cursor creation)
+    // we'd get this incorrect result:
+    //
+    // aaa!!!b
+    // ^ ^^   ^
+    // s ba   e
+
+    let pos_before = doc
+        .get_cursor_position(&text2, &before_cursor, None)
+        .unwrap();
+    let pos_after = doc
+        .get_cursor_position(&text2, &after_cursor, None)
+        .unwrap();
+    let pos_start = doc
+        .get_cursor_position_for(&text2, &start_cursor, None)
+        .unwrap();
+    let pos_end = doc
+        .get_cursor_position_for(&text2, &end_cursor, None)
+        .unwrap();
+
+    assert_eq!(pos_before, 2);
+    assert_eq!(pos_after, 6);
+    assert_eq!(pos_start, 0);
+    assert_eq!(pos_end, 7);
 
     Ok(())
 }
