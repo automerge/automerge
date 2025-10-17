658be7caa3b6fda088111ac2011951e99f0189b2
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-10-16 21:43:07 -0700

    Latest

--- rust/automerge/tests/fragment.rs
+++ rust/automerge/tests/fragment.rs
@@ -1,102 +1,193 @@
+use automerge::{transaction::Transactable, Automerge, AutomergeError, ObjType, ReadDoc, ROOT};
+
 mod fragment {
-    use std::sync::OnceLock;
+    use std::hash::{DefaultHasher, Hasher};
 
-    use automerge::{transaction::Transactable, Automerge, AutomergeError, ObjType, ReadDoc, ROOT};
+    use automerge::Automerge;
+    use testresult::TestResult;
 
-    static DOC_FIXTURE: OnceLock<Automerge> = OnceLock::new();
-    fn doc_fixture() -> &'static Automerge {
-        DOC_FIXTURE.get_or_init(|| {
-            let mut doc = Automerge::new();
-
-            doc.transact(|tx| {
-                tx.put(ROOT, "a", 1).expect("put failed");
-
-                let nested = tx
-                    .put_object(ROOT, "nested", ObjType::List)
-                    .expect("put failed");
-                tx.insert(&nested, 0, "hello").expect("put failed");
-                tx.insert(&nested, 1, "world").expect("put failed");
-
-                Ok::<_, AutomergeError>(())
-            })
-            .expect("transaction failed");
-
-            let mut left = doc.fork();
-            let mut right = doc.fork();
-
-            left.transact(|tx| {
-                tx.put(ROOT, "b", 0).expect("put failed");
-                tx.put(ROOT, "c", 0).expect("put failed");
-                Ok::<_, AutomergeError>(())
-            })
-            .expect("transaction failed");
-
-            // Right
-            right
-                .transact(|tx| {
-                    tx.put(ROOT, "c", 3).expect("put failed");
-                    tx.put(ROOT, "d", 4).expect("put failed");
-                    let (_nested_read, nested_write) = tx
-                        .get(ROOT, "nested")
-                        .expect("get failed")
-                        .expect("nested value missing at expected path");
-                    tx.insert(&nested_write, 2, "everyone").expect("put failed");
-                    Ok::<_, AutomergeError>(())
-                })
-                .expect("transaction failed");
-
-            for i in 0..100 {
-                right
-                    .transact(|tx| {
-                        tx.put(ROOT, format!("iter-{i}"), i).expect("put failed");
-                        Ok::<_, AutomergeError>(())
-                    })
-                    .expect("transaction failed");
-            }
-
-            doc.merge(&mut left).expect("merge failed");
-            doc.merge(&mut right).expect("merge failed");
-
-            doc.transact(|tx| {
-                tx.put(ROOT, "e", 5).expect("put failed");
-                tx.put(ROOT, "f", 6).expect("put failed");
-                Ok::<_, AutomergeError>(())
-            })
-            .expect("transaction failed");
-
-            for i in 0..20 {
-                doc.transact(|tx| {
-                    tx.put(ROOT, format!("iter-{i}"), i).expect("put failed");
-                    Ok::<_, AutomergeError>(())
-                })
-                .expect("transaction failed");
-            }
-
-            doc
-        })
+    fn fixture(name: &str) -> Vec<u8> {
+        std::fs::read("./tests/fixtures/".to_owned() + name).unwrap()
     }
 
     #[test]
-    fn everything_is_a_boundary() {
-        let doc = doc_fixture();
+    fn everything_is_a_boundary() -> TestResult {
+        let doc = Automerge::load(&fixture("diamond_history.automerge"))?;
         let head = doc.get_heads()[0];
-        let frag = doc.fragment(head, |_change| true).expect("FIXME");
+        let frag = doc.fragment(head, |_change| true)?;
+
+        // dbg!(frag.head_hash());
+        // dbg!(frag.boundary().keys().collect::<Vec<_>>());
+
+        assert!(frag.members().contains(&head));
+        assert!(!frag.boundary().contains_key(&head));
 
         assert_eq!(frag.head_hash(), head);
-        assert!(frag.members().contains(&head));
         assert_eq!(frag.members().len(), 1);
         assert_eq!(frag.boundary().len(), 2);
+
+        Ok(())
     }
 
-    #[test]
-    fn nothing_is_a_boundary() {
-        let doc = doc_fixture();
-        let heads = doc.get_heads();
-        let frag = doc.fragment(heads[0], |_change| false).expect("FIXME");
+    // #[test]
+    // fn nothing_is_a_boundary() -> TestResult {
+    //     let doc = Automerge::load(&fixture("diamond_history.automerge"))?;
+    //     let head = doc.get_heads()[0];
+    //     let frag = doc.fragment(head, |_change| false)?;
 
-        assert_eq!(frag.head_hash(), heads[0]);
-        assert!(frag.members().contains(&heads[0]));
-        assert_eq!(frag.members().len(), 104);
-        assert_eq!(frag.boundary().len(), 0);
+    //     assert!(frag.members().contains(&head));
+    //     assert!(!frag.boundary().contains_key(&head));
+
+    //     assert_eq!(frag.head_hash(), head);
+    //     assert_eq!(frag.members().len(), 104);
+    //     assert_eq!(frag.boundary().len(), 0);
+
+    //     Ok(())
+    // }
+
+    // #[test]
+    // fn first_hash_byte_is_zero() -> TestResult {
+    //     let doc = Automerge::load(&fixture("diamond_history.automerge"))?;
+    //     let head = doc.get_heads()[0];
+    //     let frag = doc.fragment(head, |change| {
+    //         let hash_bytes: [u8; 32] = change.hash().0;
+    //         hash_bytes[0] == 0
+    //     })?;
+
+    //     assert!(frag.members().contains(&head));
+    //     assert!(!frag.boundary().contains_key(&head));
+
+    //     assert_eq!(frag.head_hash(), head);
+    //     assert_eq!(frag.members().len(), 104);
+    //     assert_eq!(frag.boundary().len(), 0);
+
+    //     Ok(())
+    // }
+
+    // #[test]
+    // fn last_hash_byte_is_zero() -> TestResult {
+    //     let doc = Automerge::load(&fixture("diamond_history.automerge"))?;
+    //     let head = doc.get_heads()[0];
+    //     let frag = doc.fragment(head, |change| {
+    //         let hash_bytes: [u8; 32] = change.hash().0;
+    //         hash_bytes[31] == 0
+    //     })?;
+
+    //     assert!(frag.members().contains(&head));
+    //     assert!(!frag.boundary().contains_key(&head));
+
+    //     assert_eq!(frag.head_hash(), head);
+    //     assert_eq!(frag.members().len(), 23);
+    //     assert_eq!(frag.boundary().len(), 1);
+
+    //     Ok(())
+    // }
+
+    // #[test]
+    // fn hash_checksum_greater_than_200() -> TestResult {
+    //     super::gen_doc()?;
+    //     let doc = Automerge::load(&fixture("diamond_history.automerge"))?;
+    //     let head = doc.get_heads()[0];
+    //     let frag = doc.fragment(head, |change| {
+    //         dbg!(change.deps().len());
+    //         if change.deps().len() > 1 {
+    //             dbg!(change.hash());
+    //         }
+    //         false
+    //         // let hash_bytes: [u8; 32] = change.hash().0;
+    //         // let mut hasher = DefaultHasher::new();
+    //         // hasher.write(&hash_bytes);
+    //         // let silly_checksum = hasher.finish() as u8;
+    //         // silly_checksum == 219
+    //     })?;
+
+    //     let serialized = serde_json::to_string(&automerge::AutoSerde::from(&doc)).unwrap();
+    //     dbg!(serialized);
+
+    //     assert!(frag.members().contains(&head));
+    //     assert!(!frag.boundary().contains_key(&head));
+
+    //     assert_eq!(frag.head_hash(), head);
+    //     // assert_eq!(frag.members().len(), 70);
+    //     assert_eq!(frag.boundary().len(), 2);
+
+    //     Ok(())
+    // }
+}
+
+pub fn gen_doc() -> Result<Automerge, AutomergeError> {
+    let mut doc = Automerge::new();
+
+    doc.transact(|tx| {
+        tx.put(ROOT, "a", 1)?;
+
+        let nested = tx
+            .put_object(ROOT, "nested", ObjType::List)
+            .expect("put failed");
+        tx.insert(&nested, 0, "hello")?;
+        tx.insert(&nested, 1, "world")?;
+
+        Ok::<_, AutomergeError>(())
+    })
+    .map_err(|te| te.error)?;
+
+    let mut left = doc.fork();
+    let mut right = doc.fork();
+
+    doc.transact(|tx| {
+        tx.put(ROOT, "z", "left and right don't have this!")?;
+        Ok::<_, AutomergeError>(())
+    })
+    .map_err(|te| te.error)?;
+
+    left.transact(|tx| {
+        tx.put(ROOT, "b", 0)?;
+        tx.put(ROOT, "c", 0)?;
+        Ok::<_, AutomergeError>(())
+    })
+    .map_err(|te| te.error)?;
+
+    // Right
+    right
+        .transact(|tx| {
+            tx.put(ROOT, "c", 3)?;
+            tx.put(ROOT, "d", 4)?;
+            let (_nested_read, nested_write) = tx
+                .get(ROOT, "nested")?
+                .expect("nested value missing at expected path");
+            tx.insert(&nested_write, 2, "everyone")?;
+            Ok::<_, AutomergeError>(())
+        })
+        .map_err(|te| te.error)?;
+
+    for i in 0..100 {
+        right
+            .transact(|tx| {
+                tx.put(ROOT, format!("iter-{i}"), i).expect("put failed");
+                Ok::<_, AutomergeError>(())
+            })
+            .expect("transaction failed");
     }
+
+    doc.merge(&mut left)?;
+    doc.merge(&mut right)?;
+
+    doc.transact(|tx| {
+        tx.put(ROOT, "e", 5)?;
+        tx.put(ROOT, "f", 6)?;
+        Ok::<_, AutomergeError>(())
+    })
+    .map_err(|te| te.error)?;
+
+    for i in 0..20 {
+        doc.transact(|tx| {
+            tx.put(ROOT, format!("iter-{i}"), i).expect("put failed");
+            Ok::<_, AutomergeError>(())
+        })
+        .map_err(|te| te.error)?;
+    }
+
+    std::fs::write("./tests/fixtures/diamond_history.automerge", doc.save());
+
+    Ok(doc)
 }
